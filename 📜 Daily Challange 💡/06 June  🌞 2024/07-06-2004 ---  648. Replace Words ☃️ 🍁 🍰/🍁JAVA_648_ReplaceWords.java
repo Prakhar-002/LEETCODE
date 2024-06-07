592767f9d18bd2bbeccb1f92c4ac2376a73ccabd
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-06-07 08:45:16 +0530

    Inital Files

---
+++ 📜 Daily Challange 💡/06 June  🌞 2024/07-06-2004 ---  648. Replace Words ☃️ 🍁 🍰/🍁JAVA_648_ReplaceWords.java
@@ -0,0 +1,71 @@
+import java.util.StringTokenizer;
+
+class Solution {
+      
+      public static class Node {
+            Node child[] = new Node[26];
+            String str;
+      }
+
+      public static void insert(Node root, String s){
+            for (int i = 0; i < s.length(); i++) {
+                  char ch = s.charAt(i);
+                  if (root.child[ch - 'a'] == null) {
+                        root.child[ch - 'a'] = new Node();
+                  }
+
+                  root = root.child[ch - 'a'];
+                  if (root.str != null) {
+                        return;
+                  }
+            }
+
+            root.str = s;
+      }
+
+      public static String searchPrefix(Node root, String str){
+
+            for (int i = 0; i < str.length(); i++) {
+                  char ch = str.charAt(i);
+
+                  if (root.child[ch - 'a'] == null) {
+                        return null;
+                  }
+
+                  root = root.child[ch - 'a'];
+                  if (root.str != null) {
+                        return root.str;
+                  }
+            }
+
+            return null;
+      }
+
+      public String replaceWords(List<String> dictionary, String sentence) {
+            Node root = new Node();
+
+            for(String s : dictionary){
+                  insert(root, s);
+            }
+
+            StringBuilder ans = new StringBuilder();
+            StringTokenizer tokenStr = new StringTokenizer(sentence);
+
+            while (tokenStr.hasMoreTokens()) {
+                  String word = tokenStr.nextToken();
+
+                  String prefix = searchPrefix(root, word);
+                  if (prefix != null) {
+                        ans.append(prefix);
+                  } else {
+                        ans.append(word);
+                  }
+
+                  ans.append(" ");
+            }
+
+            ans.deleteCharAt(ans.length() - 1);
+
+            return ans.toString();
+      }
+}
