db73c9488ee30896b5566da72b5f283bc032650f
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-06-09 21:13:30 +0530

    🦇 Inital Files 🦋

---
+++ 🎭 LEVEL wise que with solution 🎯/🫀 Hard 2742. Painting the Walls  🍰/🍰PYTHON_2742_PaintingTheWalls.py
@@ -0,0 +1,23 @@
+class Solution:
+      def paintWalls(self, cost: List[int], time: List[int]) -> int:
+            n = len(cost)
+
+            @cache
+
+            def dfs(i, walls):
+                  # ? If remaining walls 0
+                  if walls <= 0:
+                        return 0
+                  # ? If i out of bound return infinity
+                  if i == n:
+                        return math.inf
+                  
+                  #? If we paint one wall
+                  paint = cost[i] + dfs(i + 1, walls - 1 - time[i])
+
+                  #? If we skip to paint current wall
+                  skip = dfs(i + 1, walls)
+
+                  return min(paint, skip)
+            
+            return dfs(0, n)
