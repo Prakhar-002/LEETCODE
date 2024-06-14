97b7207ebedb09f213e62ce643059b8fdd7986d2
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-06-14 10:09:35 +0530

    🦇 Inital Files 🦋

---
+++ 🎭 LEVEL wise que with solution 🎯/♻️ Easy 14. Longest Common Prefix ☃️ 🍁 🍰/🍰PYTHON_14_LongestCommonPrefix.py
@@ -0,0 +1,11 @@
+class Solution:
+      def longestCommonPrefix(self, strs: List[str]) -> str:
+            if len(strs) == 0:
+                  return ""
+
+            for i in range(len(strs[0])):
+                  for str in strs:
+                        if i == len(str) or str[i] != strs[0][i]:
+                              return strs[0][:i]
+
+            return strs[0]
