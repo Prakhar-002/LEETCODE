b1fefee6c0ba4ea0e3b35ea5f088319c213ff4d1
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-06-19 04:21:35 +0530

    ☢️ Medium ☢️

--- 🎭 LEVEL wise que with solution 🎯/☢️ Medium 106. Construct Binary Tree from Inorder & Postorder ☃️ 🍁 🍰/🍰PYTHON_106_ConstructBinaryTreeFrom_In_Postorder.py
+++ 🎭 LEVEL wise que with solution 🎯/☢️ Medium 106. Construct Binary Tree from Inorder & Postorder ☃️ 🍁 🍰/🍰PYTHON_106_ConstructBinaryTreeFrom_In_Postorder.py
@@ -1,1 +1,17 @@
- 
+class Solution:
+      def buildTree(self, inOrder: List[int], postOrder: List[int]) -> Optional[TreeNode]:
+            # If we got out inOrder empty then we'll return false 
+            if not inOrder:
+                  return None
+
+            # Making the last of postOrder to our main root
+            root = TreeNode(postOrder.pop())
+
+            # Finding position of root.val in our in inOrder array
+            idx = inOrder.index(root.val)
+            # Right of our root's value's index will be the right of of our tree
+            root.right = self.buildTree(inOrder[idx + 1:], postOrder)
+            # left of our root's value's index will be the left of of our tree
+            root.left = self.buildTree(inOrder[ : idx], postOrder)
+            # return tree
+            return root
