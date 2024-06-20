7e15c0c7d78f90f79c3121cc266cfa98ba606e9f
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-06-20 21:13:50 +0530

    ☢️ Medium ☢️

--- 📜 Daily Challange 💡/05 May 🌈 2024/03 - 05 - 2024 ---165. Compare Version Numbers 🍁/🍁JAVA_165_CompareVersionNumbers.java
+++ 📜 Daily Challange 💡/05 May 🌈 2024/03 - 05 - 2024 ---165. Compare Version Numbers 🍁/🍁JAVA_165_CompareVersionNumbers.java
@@ -2,24 +2,35 @@ class Solution {
       public int compareVersion(String version1, String version2) {
             int i = 0, j = 0;
             int len1 = version1.length(), len2 = version2.length();
-            int num1 = 0, num2 = 0;
+            // Apply loop for the whole string
             while (i < len1 || j < len2) {
-                  while (i < len1 && version1.charAt(i) != '.')
+                  int num1 = 0, num2 = 0;
+                  // Extract num up to one "." from version1
+                  while (i < len1 && version1.charAt(i) != '.') {
                         num1 = num1 * 10 + (version1.charAt(i++) - '0');
-                  while (j < len2 && version2.charAt(j) != '.')
+                  }
+                  // Extract num up to one "." from version2
+                  while (j < len2 && version2.charAt(j) != '.') {
                         num2 = num2 * 10 + (version2.charAt(j++) - '0');
-
-                  if (num1 > num2)
+                  }
+                  // if num1 is greater return 1
+                  if (num1 > num2){
                         return 1;
-                  else if (num1 < num2)
+                  }
+                  // if num2 is greater return -1
+                  else if (num1 < num2){
                         return -1;
-                  num1 = 0;
-                  num2 = 0;
-                  if (i < len1)
+                  }
+                  // inc i for skipping .
+                  if (i < len1) {
                         i++;
-                  if (j < len2)
+                  }
+                  // inc j for skipping .
+                  if (j < len2) {
                         j++;
+                  }
             }
+            // if both version are same return 0
             return 0;
       }
 }
