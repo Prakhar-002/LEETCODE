dff74a206933d2093764fdff251cd304104ff10c
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-06-20 20:55:36 +0530

    ♻️ Easy ♻️

---
+++ 📜 Daily Challange 💡/05 May 🌈 2024/02 - 05 - 2024 --- 2441. Largest Positive Integer That Exists With Its Negative 🍁/🍁JAVA_💯.java
@@ -0,0 +1,26 @@
+class Solution {
+      public int findMaxK(int[] nums) {
+            int low = Integer.MAX_VALUE;
+            int high = Integer.MIN_VALUE;
+
+            // finding the low and high
+            for (int n : nums) {
+                  low = Math.min(n, low);
+                  high = Math.max(n, high);
+            }
+            int len = Math.abs(low) + Math.abs(high) + 1;
+
+            // making an array of 2 * (min + max + 1)  
+            boolean[] visit = new boolean[2 * len];
+            int res = -1;
+            for (int n : nums) {
+                  // Check if It's positive value also present in arr
+                  if (visit[-n + len]) {
+                        res = Math.max(res, Math.abs(n));
+                  }
+                  // assume adding every -ve number
+                  visit[n + len ] = true;
+            }
+            return res;
+      }
+}
