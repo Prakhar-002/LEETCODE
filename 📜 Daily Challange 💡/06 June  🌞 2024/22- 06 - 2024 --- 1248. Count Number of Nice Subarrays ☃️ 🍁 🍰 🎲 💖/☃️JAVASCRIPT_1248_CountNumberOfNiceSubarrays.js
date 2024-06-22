8ddcb906d9a3f664bb21fe67875e63959c341d62
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-06-22 10:57:14 +0530

    ☢️ Medium ☢️

---
+++ 📜 Daily Challange 💡/06 June  🌞 2024/22- 06 - 2024 --- 1248. Count Number of Nice Subarrays ☃️ 🍁 🍰 🎲 💖/☃️JAVASCRIPT_1248_CountNumberOfNiceSubarrays.js
@@ -0,0 +1,49 @@
+var numberOfSubarrays = function(nums, k) {
+      // 3 pointer sliding window problem
+      // making 3 pointer left, middle assign with 0
+      let left = 0, middle = 0;
+      let odd = 0;
+      let subArrays = 0;
+
+      // 3rd pointer iterate whole array
+      for (let right = 0; right < nums.length; right++) {
+            // update odd if we got any odd value
+            if (nums[right] % 2 !== 0) {
+                  odd++;
+            }
+
+            // when even we get odd greater then k
+            while (odd > k) {
+                  // undo the odd value after shifting left pointer by 1
+                  if (nums[left] % 2 !== 0) {
+                        odd--;
+                  }
+                  left++;
+                  // and assign middle with left
+                  middle = left;
+            }
+
+            // Now the actual part when we got number of odd = k
+            if (odd == k) {
+                  //we'll update our middle pointer up to that index when we got our first odd value 
+                  while (nums[middle] % 2 === 0) {
+                        middle++;
+                  }
+                  // and add subArrays that can be made is => mid - left + 1
+                  // cause => [2, 2, 1, 1, 2, 1, 1]
+                  //          L               R => got 3 odd value
+                  //          M
+                  //  Update the pointer m up to first odd comes
+                  //           L  -  M  -  -  R
+                  // subArrays that can be made are
+                  //         [2, 2, 1, 1, 1, 2, 1]
+                  //         [2, 1, 1, 1, 2, 1]
+                  //         [1, 1, 1, 2, 1]
+                  // total 3 subArray that are => (M - L) + 1
+                  
+                  subArrays += (middle - left) + 1;
+            }
+      }
+
+      return subArrays;
+};
---
+++ 📜 Daily Challange 💡/06 June  🌞 2024/22- 06 - 2024 --- 1248. Count Number of Nice Subarrays ☃️ 🍁 🍰 🎲 💖/🍰PYTHON_1248_CountNumberOfNiceSubarrays.py
@@ -0,0 +1,42 @@
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        # 3 pointer sliding window problem
+        # making 3 pointer left, middle assign with 0
+        left, middle = 0, 0
+        odd = 0
+        SubArrays = 0
+
+        # 3rd pointer iterate whole array
+        for right in range(len(nums)):
+            # update odd if we got any odd value
+            if nums[right] % 2:
+                odd += 1
+
+            # when even we get odd greater then k
+            while odd > k:
+                # undo the odd value after shifting left pointer by 1
+                if nums[left] % 2:
+                    odd -= 1
+                left += 1
+                # and assign middle with left
+                middle = left
+
+            # Now the actual part when we got number of odd = k
+            if odd == k:
+                # we'll update our middle pointer up to that index when we got our first odd value
+                while not nums[middle] % 2:
+                    middle += 1
+                # and add subArrays that can be made is => mid - left + 1
+                # cause => [2, 2, 1, 1, 2, 1, 1]
+                #          L               R => got 3 odd value
+                #          M
+                #  Update the pointer m upto first odd comes
+                #           L  -  M  -  -  R
+                # subArrays that can be made are
+                #         [2, 2, 1, 1, 1, 2, 1]
+                #         [2, 1, 1, 1, 2, 1]
+                #         [1, 1, 1, 2, 1]
+                # total 3 subArray that are => (M - L) + 1
+                SubArrays += (middle - left) + 1
+
+        return SubArrays
