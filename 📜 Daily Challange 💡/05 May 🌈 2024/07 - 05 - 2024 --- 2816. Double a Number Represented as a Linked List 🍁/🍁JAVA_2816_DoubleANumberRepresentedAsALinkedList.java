2b13ca4012f788752f2a6a1ba3c965f14007f926
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-06-22 21:05:28 +0530

    ☢️ Medium ☢️

---
+++ 📜 Daily Challange 💡/05 May 🌈 2024/07 - 05 - 2024 --- 2816. Double a Number Represented as a Linked List 🍁/🍁JAVA_2816_DoubleANumberRepresentedAsALinkedList.java
@@ -0,0 +1,45 @@
+class Solution {
+      public ListNode reverse(ListNode node) {
+            ListNode prev = null;
+            ListNode cur = node;
+            ListNode next = null;
+            while (cur != null) {
+                  next = cur.next;
+                  cur.next = prev;
+                  prev = cur;
+                  cur = next;
+            }
+            node = prev;
+            return node;
+      }
+
+      public ListNode doubleIt(ListNode head) {
+
+            head = reverse(head);
+
+            ListNode headPtr = head;
+            ListNode ptr = null;
+
+            int sum = 0;
+            int carry = 0;
+
+            while (head != null) {
+                  sum = head.val * 2 + carry;
+
+                  head.val = sum % 10;
+                  carry = sum / 10 != 0 ? sum / 10 : 0;
+
+                  ptr = head;
+                  head = head.next;
+            }
+
+            if (carry != 0) {
+                  ptr.next = new ListNode(carry);
+            }
+
+            headPtr = reverse(headPtr);
+
+            return headPtr;
+
+      }
+}
