90cc479a522f3746ff683d131e7c7c06ba31c3f9
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-07-07 11:13:44 +0530

    ♻️ Easy ♻️

---
+++ 📜 Daily Challange 💡/07 July  🏖️ 2024/07 - 07 - 2024 --- 1518. Water Bottles ☃️ 🍁 🍰 🎲 💖/🍰PYTHON-1518-WaterBottles.py
@@ -0,0 +1,17 @@
+#! https://github.com/Prakhar-002/LEETCODE
+
+# Todo 📌 QUESTION NUMBER 2181
+
+#? ⌚ Time complexity -> O(LogN Of Base M) n = numBottles , m = numExchange
+
+#? 🧺 Space complexity -> O(1) 
+
+# https://github.com/Prakhar-002/LEETCODE
+
+# ⌚ Time complexity -> O(LogN Of Base M) n = numBottles , m = numExchange
+
+#  Space complexity -> O(1) 
+
+class Solution:
+      def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+            
---
+++ 📜 Daily Challange 💡/07 July  🏖️ 2024/07 - 07 - 2024 --- 1518. Water Bottles ☃️ 🍁 🍰 🎲 💖/💖C-1518-WaterBottles.c
@@ -0,0 +1,17 @@
+//! https://github.com/Prakhar-002/LEETCODE
+
+// Todo 📌 QUESTION NUMBER 2181
+
+//? ⌚ Time complexity -> O(LogN Of Base M) n = numBottles , m = numExchange
+
+//? 🧺 Space complexity -> O(1) 
+
+// https://github.com/Prakhar-002/LEETCODE
+
+// ⌚ Time complexity -> O(LogN Of Base M) n = numBottles , m = numExchange
+
+//  Space complexity -> O(1) 
+
+int numWaterBottles(int numBottles, int numExchange) {
+      
+}
