456d67c74edade8665b7d7b7dd4f3349af1d533d
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-07-15 15:44:05 +0530

    ☢️ Medium ☢️

---
+++ 📜 Daily Challange 💡/07 July  🏖️ 2024/15 - 07 - 2024 --- 2196. Create Binary Tree From Descriptions ☃️ 🍁 🍰 🎲/🍰PYTHON-2196-CreateBinaryTreeFromDescriptions.py
@@ -0,0 +1,41 @@
+#! https://github.com/Prakhar-002/LEETCODE
+
+# Todo 📌 QUESTION NUMBER 2196
+
+#? ⌚ Time complexity -> O(n) 👉 descriptions length 
+
+#? 🧺 Space complexity -> O(n) 👉 using nodes hashMap and Children HashSet
+
+class Solution:
+      def createBinaryTree(self, description):
+            treeNodes = {}
+            children = set()
+            root = None
+
+            for parent, child, isLeft in description:
+                  # add every child to our set
+                  children.add(child)
+
+                  # if parent is not yet in our hashmap create it
+                  if parent not in treeNodes:
+                        treeNodes[parent] = TreeNode(parent)
+
+                  # if child is not yet in our hashmap create it
+                  if child not in treeNodes :
+                        treeNodes[child] = TreeNode(child)
+
+                  # link our left child
+                  if isLeft :
+                        treeNodes[parent].left = treeNodes[child]
+
+                  # link our right child
+                  else :
+                        treeNodes[parent].right = treeNodes[child]
+
+            for p, c, l in description:
+                  # check our children set our root node would not we a child 
+                  if p not in children:
+                        root = treeNodes[p]
+                        break
+
+            return root
