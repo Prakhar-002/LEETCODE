41f0a50e7372169bf91b9bf06cc07505d8a75d78
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-07-25 11:46:45 +0530

    ☢️ Medium ☢️

---
+++ 📜 Daily Challange 💡/07 July  🏖️ 2024/25 - 07 - 2024 --- 912. Sort an Array ☃️ 🍁 🍰 🎲/☃️JAVASCRIPT-912-SortAnArray.js
@@ -0,0 +1,58 @@
+//! https://github.com/Prakhar-002/LEETCODE
+
+// Todo 📌 QUESTION NUMBER 912
+
+//? ⌚ Time complexity -> O(n) 👉🏻 nums length
+
+//? 🧺 Space complexity -> O(m) 👉🏻 m = max - min
+
+var sortArray = function (nums) {
+      // let 0th idx val is max and min
+      let max = nums[0];
+      let min = nums[0];
+
+      for (let num of nums) {
+            // find the max from array
+            max = Math.max(max, num);
+            // find the min from array
+            min = Math.min(min, num);
+      }
+
+      // get the length of our new array
+      let n = max - min;
+      // make the array of out length (n) which will be used to 
+      // store the frequency of each integer value in `nums`.
+      let arr = new Array(n + 1).fill(0);
+
+      for (let num of nums) {
+            // count the freq of nums's elem
+            arr[num - min]++;
+      }
+
+      let idx = 0;
+
+      // This is where the actual sorting happens. 
+      // `idx` is an index pointer for updating the input array `nums`. 
+      // The outer loop iterates through each possible value in the range between `min` and `max`. 
+      // The inner while loop runs as long as there are still occurrences (`arr[i] > 0`) of the current number (`min`). 
+      // Each time, it assigns the number `min` to `nums[idx]` and increments `idx`. 
+      // It also decrements the frequency count `arr[i]`. 
+      // After exhausting the occurrences of `min`, the value of `min` is incremented to move to the next number.
+
+      // go through the whole array 
+      for (let i = 0; i <= n; i++) {
+            // up to freq of each elem of our array
+            while (arr[i] > 0) {
+                  // assign the value 
+                  nums[idx++] = min;
+                  // dec freq
+                  arr[i]--;
+            }
+
+            // inc the value 
+            min++;
+      }
+
+      // return new array
+      return nums;
+};
