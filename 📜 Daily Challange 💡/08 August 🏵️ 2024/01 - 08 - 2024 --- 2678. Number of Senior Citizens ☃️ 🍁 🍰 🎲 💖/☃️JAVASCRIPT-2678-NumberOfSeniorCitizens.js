3307b15c9ac88e4a9627f53597f86105d2e7c406
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-08-01 13:26:07 +0530

    ♻️ Easy ♻️

--- 📜 Daily Challange 💡/08 August 🏵️ 2024/01 - 08 - 2024 --- 2678. Number of Senior Citizens ☃️ 🍁 🍰 🎲 💖/☃️JAVASCRIPT-2678-NumberOfSeniorCitizens.js
+++ 📜 Daily Challange 💡/08 August 🏵️ 2024/01 - 08 - 2024 --- 2678. Number of Senior Citizens ☃️ 🍁 🍰 🎲 💖/☃️JAVASCRIPT-2678-NumberOfSeniorCitizens.js
@@ -25,7 +25,7 @@ var countSeniors = function (details) {
       return oldPassenger;
 };
 
-// ------------------------------------------------
+//-------------------------------------------------
 
 //* One line code 
 
