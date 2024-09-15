450a41a957ed6ad13feb194d01ce8c1feaf4e6d7
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-09-15 17:28:56 +0530

    ♻️ Easy ♻️

---
+++ 🎭 LEVEL wise que with solution 🎯/♻️ Easy♻️/♻️ Easy 326. Power of Three ☃️ 🍁 🍰  🎲 💖/🍰PYTHON - 326. Power of Three.py
@@ -0,0 +1,14 @@
+#! https://github.com/Prakhar-002/LEETCODE
+
+# Todo 💎 QUESTION NUMBER 326
+
+#? ⌚ Time complexity ➺ O(1)
+
+#? 🧺 Space complexity ➺ O(1)
+
+class Solution:
+      def isPowerOfThree(self, n: int) -> bool:
+            # maximum value of an integer that is a power of 3 
+            # within the 32-bit integer range is 3^19 = 1162261467, 
+            # any number n that divides 1162261467 evenly must be a power of 3.
+            return n > 0 and 1162261467 % n == 0
---
+++ 🎭 LEVEL wise que with solution 🎯/♻️ Easy♻️/♻️ Easy 326. Power of Three ☃️ 🍁 🍰  🎲 💖/💖C - 326. Power of Three.c
@@ -0,0 +1,16 @@
+//! https://github.com/Prakhar-002/LEETCODE
+
+// Todo 💎 QUESTION NUMBER 326
+
+//? ⌚ Time complexity ➺ O(1)
+
+//? 🧺 Space complexity ➺ O(1)
+
+#include <stdbool.h>
+
+bool isPowerOfThree(int n) {
+      // maximum value of an integer that is a power of 3 
+      // within the 32-bit integer range is 3^19 = 1162261467, 
+      // any number n that divides 1162261467 evenly must be a power of 3.
+      return n > 0 && 1162261467 % n == 0;
+}
