5785640f1f9c5c6308d009e152188dda67fce247
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-09-20 22:41:31 +0530

    🫀 Hard 🫀

--- 📜 Daily Challange 💡/09 September 🍂 2024/20 - 09 - 2024 --- 214. Shortest Palindrome ☃️ 🍁 🍰 🎲/🍰PYTHON - 214. Shortest Palindrome.py
+++ 📜 Daily Challange 💡/09 September 🍂 2024/20 - 09 - 2024 --- 214. Shortest Palindrome ☃️ 🍁 🍰 🎲/🍰PYTHON - 214. Shortest Palindrome.py
@@ -1,1 +1,19 @@
- 
+#! https://github.com/Prakhar-002/LEETCODE
+
+# Todo 💎 QUESTION NUMBER 214
+
+#? ⌚ Time complexity ➺ O(n^2) 👉🏻  n = len(s)
+
+#? 🧺 Space complexity ➺ O(n)
+
+class Solution:
+      def shortestPalindrome(self, s: str) -> str:
+            i = 0
+            n = len(s)
+            for j in range(n):
+                  if s[i] == s[n-j-1]:
+                        i += 1
+            if i==n:
+                  return s
+            p = s[i:n][::-1]
+            return p + self.shortestPalindrome(s[:i]) + s[i:]
