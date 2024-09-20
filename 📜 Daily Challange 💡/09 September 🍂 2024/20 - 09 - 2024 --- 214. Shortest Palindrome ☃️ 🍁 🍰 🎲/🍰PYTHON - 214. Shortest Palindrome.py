402ee25e9102998bf9d3ba87409d1763e37ba36d
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-09-20 22:46:21 +0530

    🫀 Hard 🫀

--- 📜 Daily Challange 💡/09 September 🍂 2024/20 - 09 - 2024 --- 214. Shortest Palindrome ☃️ 🍁 🍰 🎲/🍰PYTHON - 214. Shortest Palindrome.py
+++ 📜 Daily Challange 💡/09 September 🍂 2024/20 - 09 - 2024 --- 214. Shortest Palindrome ☃️ 🍁 🍰 🎲/🍰PYTHON - 214. Shortest Palindrome.py
@@ -10,10 +10,19 @@ class Solution:
       def shortestPalindrome(self, s: str) -> str:
             i = 0
             n = len(s)
+
+            # Find the longest palindrome starting from the beginning of the string
             for j in range(n):
-                  if s[i] == s[n-j-1]:
+                  if s[i] == s[n - j - 1]:
                         i += 1
-            if i==n:
+
+            # If the entire string is a palindrome, return it
+            if i == n:
                   return s
-            p = s[i:n][::-1]
+
+            # Reverse the non-palindrome part of the string
+            p = s[i:][::-1]
+
+            # Recursively find the shortest palindrome for the remaining part
+            # and concatenate with the reversed part and the original string
             return p + self.shortestPalindrome(s[:i]) + s[i:]
