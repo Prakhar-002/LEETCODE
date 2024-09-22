bce61dd69c344b02a9a5ca3a94cbbf4f5e59bbe2
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-09-22 22:58:08 +0530

    🫀 Hard 🫀

--- 📜 Daily Challange 💡/09 September 🍂 2024/22 - 09 - 2024 --- 440. K-th Smallest in Lexicographical Order ☃️ 🍁 🍰 🎲 💖/🍰PYTHON - 440. K-th Smallest in Lexicographical Order.py
+++ 📜 Daily Challange 💡/09 September 🍂 2024/22 - 09 - 2024 --- 440. K-th Smallest in Lexicographical Order ☃️ 🍁 🍰 🎲 💖/🍰PYTHON - 440. K-th Smallest in Lexicographical Order.py
@@ -2,51 +2,62 @@
 
 # Todo 💎 QUESTION NUMBER 440
 
-#? ⌚ Time complexity ➺ O(n)
+#? ⌚ Time complexity ➺ O(log n)
 
 #? 🧺 Space complexity ➺ O(1)
 
-# https://github.com/Prakhar-002/LEETCODE
-
-# ⌚ Time complexity -> O(n) 
-
-#  Space complexity -> O(1)
-
 class Solution:
       def findKthNumber(self, n: int, k: int) -> int:
-            # to count current sequence of numbers in lexicographical order
-            count = 1
-
-            # used to generate the next number in lexicographical order
+            # `cur` is initialized to 1, 
+            # which is our starting point in lexicographical order
             cur = 1
+            # `i` is initialized to 1, 
+            # representing the first step in our search
+            i = 1
 
-            # until the count is less than `n`
-            # ensures that exactly `n` numbers are added to the list
-            while count < n:
-                  # checking the lexicographical order current elem's number
-                  if k == count:
-                        # return current lexicographical element
-                        return cur
+            def stepCount(cur):
+                  # `steps` initializes at 0, 
+                  # which will accumulate the count of valid numbers
+                  steps = 0
+                  # `first` will start from the current prefix
+                  first = cur
+                  # `last` is the next prefix in the sequence
+                  last = cur + 1
 
-                  # inc count per next lexicographical number
-                  count += 1
+                  #  loop breaks when `first` goes beyond `n`
+                  while first <= n:
+                        # calculates the number of steps 
+                        # or numbers between 'cur' and the next prefix
+                        steps += min(last, n + 1) - first
+                        # explore numbers within the range 
+                        # while multiplying by 10 for each iteration
+                        first *= 10
+                        last *= 10
+                  return steps
 
-                  # `cur * 10` is less than or equal to `n`
-                  if cur * 10 <= n:
-                        # `cur` is multiplied by 10
-                        # moves to the next level of depth in the lexicographical tree 
-                        # (e.g., from 1 to 10, from 2 to 20, etc.)
-                        cur *= 10
-                  else:
-                        # `cur * 10` is greater than `n`, the code enters another while loop
-                        while cur == n or cur % 10 == 9:
-                              # as long as `cur` equals `n` or the last digit of `cur` is 9. 
-                              # In each iteration of this loop, `cur` is integer-divided by 10
-                              # moves back to check previous numbers for possible lexicographical increments
-                              cur //= 10
-                        # `cur` is incremented by 1. This is akin to moving to the next sibling in a tree structure 
-                        # (e.g., from 19 to 20).
+            # Runs until we reach the k-th number
+            while i < k:
+                  # `stepCount(cur)` is used to determine the 
+                  # number of lexicographical numbers from the current `cur`
+                  steps = stepCount(cur)
+
+                  # If adding `steps` to `i` would overshoot `k`
+                  # it means the k-th number must be found by 
+                  # going deeper into the current prefix
+                  if i + steps <= k:
+                        # If it does not overshoot, it points to the fact that 
+                        # the k-th number is not within this deeper level
+
+                        # `cur` is incremented by 1 to move to the next prefix
+                        # (e.g., from `1` to `2` if initially 1 had been the current)
                         cur += 1
-
-            # Return the list
+                        #  Increment `i` by `steps` since all those steps are now included up to the next prefix
+                        i += steps
+                  else:
+                        # `cur` is multiplied by 10 
+                        # to go one level deeper in the lexicographical tree 
+                        # (e.g., from 1 to 10, 10 to 100)
+                        cur *= 10
+                        #  Increment `i` since it now includes the current `cur` number itself
+                        i += 1
             return cur
