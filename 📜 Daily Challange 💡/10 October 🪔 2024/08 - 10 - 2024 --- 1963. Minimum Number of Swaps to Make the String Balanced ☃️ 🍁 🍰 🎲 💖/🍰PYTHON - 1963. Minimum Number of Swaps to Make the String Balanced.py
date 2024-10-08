9e1e495915b439e158130635201eec841b8b8c9a
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-10-08 10:47:52 +0530

    ☢️ Medium ☢️

---
+++ 📜 Daily Challange 💡/10 October 🪔 2024/08 - 10 - 2024 --- 1963. Minimum Number of Swaps to Make the String Balanced ☃️ 🍁 🍰 🎲 💖/🍰PYTHON - 1963. Minimum Number of Swaps to Make the String Balanced.py
@@ -0,0 +1,17 @@
+#! https://github.com/Prakhar-002/LEETCODE
+
+# Todo 💎 QUESTION NUMBER 1963
+
+#? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(s)
+
+#? 🧺 Space complexity ➺ O(1)
+
+# https://github.com/Prakhar-002/LEETCODE
+
+# ⌚ Time complexity -> O(n) ->  n = len(s)
+
+#  Space complexity -> O(1)
+
+class Solution:
+      def minSwaps(self, s: str) -> int:
+            
