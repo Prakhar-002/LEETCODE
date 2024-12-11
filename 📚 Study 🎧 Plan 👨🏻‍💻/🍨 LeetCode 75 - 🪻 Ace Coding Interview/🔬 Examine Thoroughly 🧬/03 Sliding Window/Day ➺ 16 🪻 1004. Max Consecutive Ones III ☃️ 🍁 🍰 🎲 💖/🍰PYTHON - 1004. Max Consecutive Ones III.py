53cc093b8d134d473f280efce2768b0bc5de5522
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-12-12 04:10:04 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/🍨 LeetCode 75 - 🪻 Ace Coding Interview/🔬 Examine Thoroughly 🧬/03 Sliding Window/Day ➺ 16 🪻 1004. Max Consecutive Ones III ☃️ 🍁 🍰 🎲 💖/🍰PYTHON - 1004. Max Consecutive Ones III.py
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🍨 LeetCode 75 - 🪻 Ace Coding Interview/🔬 Examine Thoroughly 🧬/03 Sliding Window/Day ➺ 16 🪻 1004. Max Consecutive Ones III ☃️ 🍁 🍰 🎲 💖/🍰PYTHON - 1004. Max Consecutive Ones III.py
@@ -1,6 +1,6 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER 1004
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
 
@@ -11,3 +11,7 @@
 # ⌚ Time complexity -> O(n) ->  n = len(nums)
 
 #  Space complexity -> O(1)
+
+class Solution:
+      def longestOnes(self, nums: List[int], k: int) -> int:
+            
