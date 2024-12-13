2107396c961993d5449126c40318d83ec4778a1a
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2024-12-13 20:59:03 +0530

    ♻️ Easy ♻️

---
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🍨 LeetCode 75 - 🪻 Ace Coding Interview/🔬 Examine Thoroughly 🧬/04 Prefix Sum/Day ➺ 18 🪻 1732. Find the Highest Altitude ☃️ 🍁 🍰 🎲 💖/💖C - 1732. Find the Highest Altitude.c
@@ -0,0 +1,32 @@
+//! https://github.com/Prakhar-002/LEETCODE
+
+// Todo 💎 QUESTION NUMBER 1732
+
+//? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(gain)
+
+//? 🧺 Space complexity ➺ O(1)
+
+#include <stdio.h>
+#include <limits.h>
+
+int largestAltitude(int* gain, int gainSize) {
+      // Initialize the highest altitude to negative infinity
+      int highAltitude = INT_MIN;
+
+      // Initialize the current altitude to 0
+      int curSum = 0;
+
+      // Iterate through the gain array to calculate altitudes
+      for (int i = 0; i < gainSize; i++) {
+            // Add the current gain to the cumulative altitude
+            curSum += gain[i];
+
+            // Update the highest altitude encountered so far
+            if (curSum > highAltitude) {
+                  highAltitude = curSum;
+            }
+      }
+
+      // If the highest altitude is positive, return it; otherwise, return 0
+      return highAltitude > 0 ? highAltitude : 0;
+}
