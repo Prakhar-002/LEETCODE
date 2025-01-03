5bf8f48dcf0894dd1fb35fd9a8a7fcdc2e04f379
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-03 17:36:05 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/🍨 LeetCode 75 - 🪻 Ace Coding Interview/🔬 Examine Thoroughly 🧬/08 Linked List/Day ➺ 30 🪻 328. Odd Even Linked List ☃️ 🍁 🍰 🎲 💖/🍰PYTHON - 328. Odd Even Linked List.py
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🍨 LeetCode 75 - 🪻 Ace Coding Interview/🔬 Examine Thoroughly 🧬/08 Linked List/Day ➺ 30 🪻 328. Odd Even Linked List ☃️ 🍁 🍰 🎲 💖/🍰PYTHON - 328. Odd Even Linked List.py
@@ -6,8 +6,25 @@
 
 #? 🧺 Space complexity ➺ O(1)
 
-# https://github.com/Prakhar-002/LEETCODE
+class Solution:
+      def oddEvenList(self, head):
+            if not head or not head.next:
+                  # If the list is empty or has only one node, return as is
+                  return head
 
-# ⌚ Time complexity -> O(n) ->  n = No. of nodes
+            # Initialize pointers for odd and even lists
+            odd = head
+            even = head.next
+            evenHead = even  # Store the head of the even list
 
-#  Space complexity -> O(1)
+            # Traverse the list, separating odd and even nodes
+            while even and even.next:
+                  odd.next = even.next  # Connect odd nodes
+                  odd = odd.next  # Move odd pointer
+                  even.next = odd.next  # Connect even nodes
+                  even = even.next  # Move even pointer
+
+            # Attach the even list to the end of the odd list
+            odd.next = evenHead
+
+            return head  # Return the modified head of the list
--- 📚 Study 🎧 Plan 👨🏻‍💻/🍨 LeetCode 75 - 🪻 Ace Coding Interview/🔬 Examine Thoroughly 🧬/08 Linked List/Day ➺ 30 🪻 328. Odd Even Linked List ☃️ 🍁 🍰 🎲 💖/💖C - 328. Odd Even Linked List.c
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🍨 LeetCode 75 - 🪻 Ace Coding Interview/🔬 Examine Thoroughly 🧬/08 Linked List/Day ➺ 30 🪻 328. Odd Even Linked List ☃️ 🍁 🍰 🎲 💖/💖C - 328. Odd Even Linked List.c
@@ -5,3 +5,35 @@
 //? ⌚ Time complexity ➺ O(n) 👉🏻  n = No. of nodes
 
 //? 🧺 Space complexity ➺ O(1)
+
+#include <stdlib.h>
+
+struct ListNode {
+      int val;
+      struct ListNode* next;
+};
+
+struct ListNode* oddEvenList(struct ListNode* head) {
+      if (!head || !head->next) {
+            // If the list is empty or has only one node, return as is
+            return head;
+      }
+
+      // Initialize pointers for odd and even lists
+      struct ListNode* odd = head;
+      struct ListNode* even = head->next;
+      struct ListNode* evenHead = even; // Store the head of the even list
+
+      // Traverse the list, separating odd and even nodes
+      while (even && even->next) {
+            odd->next = even->next; // Connect odd nodes
+            odd = odd->next; // Move odd pointer
+            even->next = odd->next; // Connect even nodes
+            even = even->next; // Move even pointer
+      }
+
+      // Attach the even list to the end of the odd list
+      odd->next = evenHead;
+
+      return head; // Return the modified head of the list
+}
