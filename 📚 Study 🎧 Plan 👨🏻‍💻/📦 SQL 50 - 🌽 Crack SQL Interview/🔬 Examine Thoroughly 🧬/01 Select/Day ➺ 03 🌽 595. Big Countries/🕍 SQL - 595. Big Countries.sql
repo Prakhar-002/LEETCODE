4fe968f60097a2716651d7a356b51ad1b27debf8
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-06 02:13:30 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/01 Select/Day ➺ 03 🌽 595. Big Countries/🕍 SQL - 595. Big Countries.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/01 Select/Day ➺ 03 🌽 595. Big Countries/🕍 SQL - 595. Big Countries.sql
@@ -1,3 +1,7 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 595
+
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000
