e622f630f3f829da4d218f6add69a1f2d0e4e045
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-06 01:54:43 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/01 Select/Day ➺ 02 🌽 584. Find Customer Referee/SQL - 584. Find Customer Referee .sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/01 Select/Day ➺ 02 🌽 584. Find Customer Referee/SQL - 584. Find Customer Referee .sql
@@ -1,3 +1,8 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 584
+
+SELECT name 
+FROM Customer
+WHERE referee_id != 2
+or referee_id IS NULL
