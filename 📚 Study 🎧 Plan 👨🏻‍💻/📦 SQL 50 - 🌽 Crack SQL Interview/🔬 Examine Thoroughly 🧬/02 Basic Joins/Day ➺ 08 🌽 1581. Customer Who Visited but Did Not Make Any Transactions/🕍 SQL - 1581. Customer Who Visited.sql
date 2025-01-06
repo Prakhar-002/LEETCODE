82a0fbfc1cfff0f3518a9e2a2db6bea22a308940
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-06 12:58:00 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/02 Basic Joins/Day ➺ 08 🌽 1581. Customer Who Visited but Did Not Make Any Transactions/🕍 SQL - 1581. Customer Who Visited.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/02 Basic Joins/Day ➺ 08 🌽 1581. Customer Who Visited but Did Not Make Any Transactions/🕍 SQL - 1581. Customer Who Visited.sql
@@ -1,3 +1,10 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1581
+
+SELECT v.customer_id , COUNT(v.visit_id) as count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id
