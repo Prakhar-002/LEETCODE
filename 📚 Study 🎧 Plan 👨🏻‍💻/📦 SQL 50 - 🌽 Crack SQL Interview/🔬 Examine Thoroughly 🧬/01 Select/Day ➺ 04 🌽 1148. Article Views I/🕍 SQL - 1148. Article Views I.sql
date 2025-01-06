81074a3a497411f5b4acfb67f4b5c54eabaea720
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-06 12:10:14 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/01 Select/Day ➺ 04 🌽 1148. Article Views I/🕍 SQL - 1148. Article Views I.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/01 Select/Day ➺ 04 🌽 1148. Article Views I/🕍 SQL - 1148. Article Views I.sql
@@ -1,3 +1,8 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1148
+
+SELECT DISTINCT author_id as id
+FROM Views
+WHERE author_id = viewer_id 
+ORDER BY author_id
