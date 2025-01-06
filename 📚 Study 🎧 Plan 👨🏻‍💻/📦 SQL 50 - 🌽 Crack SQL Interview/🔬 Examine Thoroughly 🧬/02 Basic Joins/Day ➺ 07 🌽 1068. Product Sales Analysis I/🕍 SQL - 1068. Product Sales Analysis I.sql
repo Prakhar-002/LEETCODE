7e3cf687bc400ba285eed09e80b31c1c2d6e5fab
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-06 12:44:53 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/02 Basic Joins/Day ➺ 07 🌽 1068. Product Sales Analysis I/🕍 SQL - 1068. Product Sales Analysis I.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/02 Basic Joins/Day ➺ 07 🌽 1068. Product Sales Analysis I/🕍 SQL - 1068. Product Sales Analysis I.sql
@@ -1,3 +1,8 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1068
+
+SELECT product_name, year, price
+FROM sales s
+JOIN product p
+on p.product_id = s.product_id 
---
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/02 Basic Joins/Day ➺ 08 🌽 1581. Customer Who Visited but Did Not Make Any Transactions/🕍 SQL - 1581. Customer Who Visited.sql
@@ -0,0 +1,3 @@
+--! https://github.com/Prakhar-002/LEETCODE
+
+--Todo 💎 QUESTION NUMBER ----
