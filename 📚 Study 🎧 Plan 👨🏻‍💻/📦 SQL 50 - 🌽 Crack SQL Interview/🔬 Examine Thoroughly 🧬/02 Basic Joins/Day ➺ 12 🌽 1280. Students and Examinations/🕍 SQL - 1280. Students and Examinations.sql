03f6c8a634d2b8f8018bd099bd2daa43a6c887f1
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-06 14:41:01 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/02 Basic Joins/Day ➺ 12 🌽 1280. Students and Examinations/🕍 SQL - 1280. Students and Examinations.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/02 Basic Joins/Day ➺ 12 🌽 1280. Students and Examinations/🕍 SQL - 1280. Students and Examinations.sql
@@ -1,3 +1,13 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1280 
+
+SELECT s.student_id, s.student_name, sub.subject_name, 
+COUNT(e.subject_name) AS attended_exams
+FROM Students s 
+CROSS JOIN Subjects sub 
+LEFT JOIN Examinations e 
+on e.student_id = s.student_id
+and e.subject_name = sub.subject_name
+GROUP BY  s.student_id, s.student_name, sub.subject_name
+ORDER BY  s.student_id, s.student_name
