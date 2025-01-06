a4411cd6d9abd60d89ebcf8e4fc87dd73befd32a
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-06 12:34:03 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/02 Basic Joins/Day ➺ 06 🌽 1378. Replace Employee ID With The Unique Identifier/🕍 SQL - 1378. Replace Employee ID With The.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/02 Basic Joins/Day ➺ 06 🌽 1378. Replace Employee ID With The Unique Identifier/🕍 SQL - 1378. Replace Employee ID With The.sql
@@ -1,3 +1,8 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1378
+
+SELECT eUNI.unique_id , emp.name
+FROM Employees emp
+LEFT JOIN EmployeeUNI eUNI
+on emp.id = eUNI.id
