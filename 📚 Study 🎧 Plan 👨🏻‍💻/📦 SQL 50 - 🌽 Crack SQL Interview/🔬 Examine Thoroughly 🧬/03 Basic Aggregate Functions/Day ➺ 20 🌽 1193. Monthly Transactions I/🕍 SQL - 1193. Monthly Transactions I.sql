76d5aeb66ed5a537c610d963b23ce51acec9de18
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-07 12:55:31 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 20 🌽 1193. Monthly Transactions I/🕍 SQL - 1193. Monthly Transactions I.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 20 🌽 1193. Monthly Transactions I/🕍 SQL - 1193. Monthly Transactions I.sql
@@ -1,3 +1,14 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1193
+
+SELECT 
+-- LEFT(trans_date , 7) AS month , (or)
+DATE_FORMAT(trans_date, '%Y-%m') AS month ,
+country, 
+COUNT(id) AS trans_count ,
+SUM(IF(state = 'approved', 1, 0)) AS approved_count,
+SUM(amount) AS trans_total_amount ,
+SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+FROM Transactions 
+GROUP BY month, country 
