be3b0e3fe61b1382e27f76ee3c2bdfa2bf84218a
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-07 05:41:30 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/02 Basic Joins/Day ➺ 14 🌽 1934. Confirmation Rate/🕍 SQL - 1934. Confirmation Rate.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/02 Basic Joins/Day ➺ 14 🌽 1934. Confirmation Rate/🕍 SQL - 1934. Confirmation Rate.sql
@@ -1,3 +1,10 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1934
+
+SELECT s.user_id ,
+ROUND(AVG (IF(c.action = 'confirmed ', 1, 0)), 2) AS  confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c 
+ON s.user_id = c.user_id
+GROUP BY s.user_id
