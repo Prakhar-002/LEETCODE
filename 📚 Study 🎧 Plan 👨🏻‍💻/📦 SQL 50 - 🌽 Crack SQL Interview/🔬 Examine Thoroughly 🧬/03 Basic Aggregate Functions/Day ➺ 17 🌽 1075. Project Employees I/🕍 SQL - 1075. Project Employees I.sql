477a7a98da8488c78a407e9eddfc07a94be785c2
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-07 06:08:36 +0530

    ♻️ Easy ♻️

---
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 17 🌽 1075. Project Employees I/🕍 SQL - 1075. Project Employees I.sql
@@ -0,0 +1,10 @@
+--! https://github.com/Prakhar-002/LEETCODE
+
+--Todo 💎 QUESTION NUMBER 1075
+
+SELECT p.project_id,
+      ROUND(AVG(e.experience_years ), 2) AS average_years
+FROM Project p
+      LEFT JOIN Employee e
+      ON p.employee_id = e.employee_id
+GROUP BY p. project_id
