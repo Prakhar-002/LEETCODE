a5bed4c44a5161587b7558b972909b2e8a7bb28b
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-07 12:35:34 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 19 🌽 1211. Queries Quality and Percentage/🕍 SQL - 1211. Queries Quality and Percentage.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 19 🌽 1211. Queries Quality and Percentage/🕍 SQL - 1211. Queries Quality and Percentage.sql
@@ -1,3 +1,10 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1211
+
+SELECT query_name ,
+      ROUND(AVG(rating / position), 2) AS quality ,
+      ROUND(AVG(IF(rating < 3, 1, 0) * 100), 2) AS poor_query_percentage
+FROM queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name
