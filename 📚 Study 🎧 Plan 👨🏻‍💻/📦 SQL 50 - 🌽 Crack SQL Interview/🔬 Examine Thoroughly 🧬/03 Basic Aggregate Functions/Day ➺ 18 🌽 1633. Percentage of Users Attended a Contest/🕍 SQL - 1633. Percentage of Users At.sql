e4729ae7f1d733d55cf90a19a77c33563ae1b2bd
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-07 06:23:48 +0530

    ♻️ Easy ♻️

---
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 18 🌽 1633. Percentage of Users Attended a Contest/🕍 SQL - 1633. Percentage of Users At.sql
@@ -0,0 +1,12 @@
+--! https://github.com/Prakhar-002/LEETCODE
+
+--Todo 💎 QUESTION NUMBER 1633
+
+SELECT contest_id ,
+      ROUND(COUNT(DISTINCT user_id) * 100 / (
+            SELECT COUNT(user_id)
+                  FROM users
+      ) , 2) AS  percentage
+FROM Register
+GROUP BY contest_id 
+ORDER BY percentage DESC , contest_id ASC
