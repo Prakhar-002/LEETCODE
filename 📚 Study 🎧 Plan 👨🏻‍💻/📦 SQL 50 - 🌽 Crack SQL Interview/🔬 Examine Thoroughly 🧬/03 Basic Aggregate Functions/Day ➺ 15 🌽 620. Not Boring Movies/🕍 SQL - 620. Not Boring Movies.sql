3467ff6e3719baf1dd05b7e89c957618103b57d3
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-07 05:45:14 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 15 🌽 620. Not Boring Movies/🕍 SQL - 620. Not Boring Movies.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 15 🌽 620. Not Boring Movies/🕍 SQL - 620. Not Boring Movies.sql
@@ -1,3 +1,8 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 620
+
+SELECT * FROM cinema 
+WHERE id % 2 != 0
+AND description != 'boring'
+ORDER BY rating DESC
