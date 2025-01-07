b771d651af288b724c8767627ad5dcd951d51340
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-07 06:00:34 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 16 🌽 1251. Average Selling Price/🕍 SQL - 1251. Average Selling Price.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 16 🌽 1251. Average Selling Price/🕍 SQL - 1251. Average Selling Price.sql
@@ -1,3 +1,11 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1251
+
+SELECT p.product_id ,
+      IFNULL(ROUND(SUM(p.price * u.units) / SUM(u.units) ,2), 0) AS average_price
+FROM Prices p
+      LEFT JOIN UnitsSold u
+      ON p.product_id = u.product_id
+            AND u.purchase_date BETWEEN p.start_date and p.end_date
+GROUP BY p.product_id
