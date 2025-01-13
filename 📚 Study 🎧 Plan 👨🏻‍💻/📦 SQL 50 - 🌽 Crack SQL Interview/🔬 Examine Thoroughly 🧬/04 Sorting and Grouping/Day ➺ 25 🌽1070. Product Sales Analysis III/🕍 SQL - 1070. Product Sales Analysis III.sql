030f38c0fe6de8443695b107da684ae452d7d705
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-13 14:48:06 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/04 Sorting and Grouping/Day ➺ 25 🌽1070. Product Sales Analysis III/🕍 SQL - 1070. Product Sales Analysis III.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/04 Sorting and Grouping/Day ➺ 25 🌽1070. Product Sales Analysis III/🕍 SQL - 1070. Product Sales Analysis III.sql
@@ -1,3 +1,16 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1070
+
+SELECT
+      product_id,
+      year AS first_year, quantity, price
+FROM sales
+WHERE ((product_id, year) IN 
+(
+SELECT 
+      product_id, 
+      MIN(year)
+FROM sales
+GROUP BY product_id
+))
