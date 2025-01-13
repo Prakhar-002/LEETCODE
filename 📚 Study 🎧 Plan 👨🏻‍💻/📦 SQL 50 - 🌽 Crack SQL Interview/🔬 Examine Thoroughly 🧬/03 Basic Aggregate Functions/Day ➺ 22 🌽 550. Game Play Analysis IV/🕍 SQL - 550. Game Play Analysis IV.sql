c60abf6e2e0a65f66001652c607dc868bf4d8a76
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-13 14:14:51 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 22 🌽 550. Game Play Analysis IV/🕍 SQL - 550. Game Play Analysis IV.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 22 🌽 550. Game Play Analysis IV/🕍 SQL - 550. Game Play Analysis IV.sql
@@ -1,3 +1,11 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 550
+
+SELECT 
+      ROUND(SUM(player_login) / COUNT(DISTINCT player_id), 2) AS fraction
+FROM
+(SELECT 
+      player_id,
+      DATEDIFF(event_date, MIN(event_date) OVER(PARTITION BY player_id)) = 1 AS player_login
+FROM activity) AS new_table
