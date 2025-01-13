fc9ad86ba7d5814f37f2f00f099fe992eba16a04
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-13 16:48:17 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 30 🌽1731. The Number of Employees Which Report to Each Employee/🕍 SQL - 1731. The Numb.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 30 🌽1731. The Number of Employees Which Report to Each Employee/🕍 SQL - 1731. The Numb.sql
@@ -1,3 +1,13 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1731
+
+SELECT e1.employee_id,
+      e1.name,
+      COUNT(e2.reports_to) AS reports_count,
+      ROUND(AVG(e2.age)) AS average_age
+FROM Employees e1
+      INNER JOIN Employees e2
+      ON e1.employee_id = e2.reports_to
+GROUP BY e1.employee_id
+ORDER BY e1.employee_id
