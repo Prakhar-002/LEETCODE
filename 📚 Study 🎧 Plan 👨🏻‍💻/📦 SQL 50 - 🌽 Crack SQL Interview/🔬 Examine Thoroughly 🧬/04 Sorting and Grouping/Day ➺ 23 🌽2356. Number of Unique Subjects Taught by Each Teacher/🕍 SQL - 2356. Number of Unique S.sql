74e1461fa1c074591dd66469b90aa3c715380f69
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-13 14:20:53 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/04 Sorting and Grouping/Day ➺ 23 🌽2356. Number of Unique Subjects Taught by Each Teacher/🕍 SQL - 2356. Number of Unique S.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/04 Sorting and Grouping/Day ➺ 23 🌽2356. Number of Unique Subjects Taught by Each Teacher/🕍 SQL - 2356. Number of Unique S.sql
@@ -1,3 +1,9 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 2356
+
+SELECT 
+      teacher_id ,
+      COUNT(DISTINCT subject_id) AS cnt
+FROM teacher 
+GROUP BY teacher_id
