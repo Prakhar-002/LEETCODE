22d127cc1a26f8c6495e0f272fdfdfda8a3d87cd
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-13 14:33:13 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/04 Sorting and Grouping/Day ➺ 24 🌽1141. User Activity for the Past 30 Days I/🕍 SQL - 1141. User Activity for the Past 30 .sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/04 Sorting and Grouping/Day ➺ 24 🌽1141. User Activity for the Past 30 Days I/🕍 SQL - 1141. User Activity for the Past 30 .sql
@@ -1,3 +1,10 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1141
+
+SELECT 
+      activity_date AS day,
+      COUNT(DISTINCT user_id ) AS active_users
+FROM Activity 
+WHERE activity_date BETWEEN date_sub('2019-07-27', interval 29 day) AND '2019-07-27'
+GROUP BY day
