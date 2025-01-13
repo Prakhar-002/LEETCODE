1567b8ddbba69e126d0461961a2fccd99dbbc95c
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-13 13:59:34 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 21 🌽 1174. Immediate Food Delivery II/🕍 SQL - 1174. Immediate Food Delivery II.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/03 Basic Aggregate Functions/Day ➺ 21 🌽 1174. Immediate Food Delivery II/🕍 SQL - 1174. Immediate Food Delivery II.sql
@@ -1,3 +1,14 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1174
+
+SELECT 
+ROUND(SUM(IF(min_order_date = min_customer_pref_delivery_date, 1, 0) * 100) / COUNT(min_order_date), 2) AS immediate_percentage
+FROM
+      (SELECT
+            delivery_id,
+            customer_id,
+            min(order_date) AS min_order_date,
+            min(customer_pref_delivery_date) AS min_customer_pref_delivery_date
+      FROM Delivery
+      GROUP BY customer_id) as new_table
