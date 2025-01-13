abc8e2967b4d586d4637c509dc603e7aefe42e76
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-13 14:57:38 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/04 Sorting and Grouping/Day ➺ 27 🌽1729. Find Followers Count/🕍 SQL - 1729. Find Followers Count.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/04 Sorting and Grouping/Day ➺ 27 🌽1729. Find Followers Count/🕍 SQL - 1729. Find Followers Count.sql
@@ -1,3 +1,10 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1729
+
+SELECT 
+      user_id,
+      COUNT(follower_id ) AS followers_count
+FROM Followers 
+GROUP BY user_id 
+ORDER BY user_id
