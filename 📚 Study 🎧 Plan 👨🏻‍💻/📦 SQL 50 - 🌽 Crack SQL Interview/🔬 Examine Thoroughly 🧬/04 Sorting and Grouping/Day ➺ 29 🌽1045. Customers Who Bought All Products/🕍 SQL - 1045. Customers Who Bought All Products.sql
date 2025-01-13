ce4c17f211622a272901e602ead9e6c142b0533a
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-13 15:18:39 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/04 Sorting and Grouping/Day ➺ 29 🌽1045. Customers Who Bought All Products/🕍 SQL - 1045. Customers Who Bought All Products.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/04 Sorting and Grouping/Day ➺ 29 🌽1045. Customers Who Bought All Products/🕍 SQL - 1045. Customers Who Bought All Products.sql
@@ -1,3 +1,8 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1045
+
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(product_key) FROM Product)
