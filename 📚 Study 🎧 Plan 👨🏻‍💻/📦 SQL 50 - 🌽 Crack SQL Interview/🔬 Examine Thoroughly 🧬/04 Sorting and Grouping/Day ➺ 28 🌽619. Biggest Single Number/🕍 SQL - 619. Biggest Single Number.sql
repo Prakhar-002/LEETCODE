1150a5b60f1b33cca5dc754a7dca9dc097129438
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-13 15:02:29 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/04 Sorting and Grouping/Day ➺ 28 🌽619. Biggest Single Number/🕍 SQL - 619. Biggest Single Number.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/04 Sorting and Grouping/Day ➺ 28 🌽619. Biggest Single Number/🕍 SQL - 619. Biggest Single Number.sql
@@ -1,3 +1,9 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 619
+
+SELECT MAX(num) AS num FROM 
+(SELECT num 
+FROM MyNumbers 
+GROUP BY num
+HAVING COUNT(num) = 1) AS distinct_num
