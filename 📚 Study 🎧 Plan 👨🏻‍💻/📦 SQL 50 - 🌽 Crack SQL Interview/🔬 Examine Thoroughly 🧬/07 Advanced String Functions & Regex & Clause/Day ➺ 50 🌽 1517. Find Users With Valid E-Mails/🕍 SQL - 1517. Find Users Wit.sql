0dcf55a61055a2e89e88c60c9a2324e04d92eb72
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 19:29:49 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/07 Advanced String Functions & Regex & Clause/Day ➺ 50 🌽 1517. Find Users With Valid E-Mails/🕍 SQL - 1517. Find Users Wit.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/07 Advanced String Functions & Regex & Clause/Day ➺ 50 🌽 1517. Find Users With Valid E-Mails/🕍 SQL - 1517. Find Users Wit.sql
@@ -1,3 +1,6 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1517
+
+SELECT * FROM Users
+WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com$'
