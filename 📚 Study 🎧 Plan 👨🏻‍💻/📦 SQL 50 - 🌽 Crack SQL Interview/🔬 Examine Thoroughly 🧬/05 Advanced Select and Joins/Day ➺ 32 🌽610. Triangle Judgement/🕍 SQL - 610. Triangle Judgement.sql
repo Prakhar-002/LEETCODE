c41462a1746ec389c237d50e603bf15097c5440e
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 16:03:50 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 32 🌽610. Triangle Judgement/🕍 SQL - 610. Triangle Judgement.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 32 🌽610. Triangle Judgement/🕍 SQL - 610. Triangle Judgement.sql
@@ -1,3 +1,13 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 610
+
+SELECT x , y , z ,
+CASE WHEN 
+      x + y > z 
+      AND 
+      y + z > x 
+      AND 
+      z + x > y 
+      THEN 'Yes' ELSE 'No' END AS triangle
+FROM Triangle 
