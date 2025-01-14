aea763a3cc1a93be1af42657a78d6ac81675b1b6
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 18:04:07 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/07 Advanced String Functions & Regex & Clause/Day ➺ 45 🌽1527. Patients With a Condition/🕍 SQL - 1527. Patients With a Con.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/07 Advanced String Functions & Regex & Clause/Day ➺ 45 🌽1527. Patients With a Condition/🕍 SQL - 1527. Patients With a Con.sql
@@ -1,3 +1,8 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1527
+
+SELECT *
+FROM Patients 
+WHERE conditions LIKE 'DIAB1%'
+OR conditions LIKE '% DIAB1%'
