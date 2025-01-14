28ce39c2284c1eaab8d76dde840421eced7cee76
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 16:39:04 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 36 🌽1907. Count Salary Categories/🕍 SQL - 1907. Count Salary Categories.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 36 🌽1907. Count Salary Categories/🕍 SQL - 1907. Count Salary Categories.sql
@@ -1,3 +1,19 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1907
+
+SELECT "Low Salary" AS category ,
+SUM(IF(income < 20000, 1, 0)) AS accounts_count 
+FROM Accounts
+
+UNION 
+
+SELECT "Average Salary" AS category ,
+SUM(IF(income BETWEEN 20000 AND 50000, 1, 0)) AS accounts_count 
+FROM Accounts
+
+UNION 
+
+SELECT "High Salary" AS category ,
+SUM(IF(income > 50000, 1, 0)) AS accounts_count 
+FROM Accounts
