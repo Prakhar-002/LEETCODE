47e01423655dddab42a15b441927599e2dd3e9f9
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 17:59:29 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/07 Advanced String Functions & Regex & Clause/Day ➺ 44 🌽1667. Fix Names in a Table/🕍 SQL - 1667. Fix Names in a Table.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/07 Advanced String Functions & Regex & Clause/Day ➺ 44 🌽1667. Fix Names in a Table/🕍 SQL - 1667. Fix Names in a Table.sql
@@ -1,3 +1,8 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1667
+
+SELECT user_id ,
+CONCAT(UPPER(LEFT(name, 1)), LOWER(RIGHT(name, LENGTH(name) - 1))) AS name 
+FROM Users 
+ORDER BY user_id
