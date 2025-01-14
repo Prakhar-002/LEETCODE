734d00ba5bba15519588ce106a161646d086bd81
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 18:17:43 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/07 Advanced String Functions & Regex & Clause/Day ➺ 48 🌽 1484. Group Sold Products By The Date/🕍 SQL - 1484. Group Sold P.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/07 Advanced String Functions & Regex & Clause/Day ➺ 48 🌽 1484. Group Sold Products By The Date/🕍 SQL - 1484. Group Sold P.sql
@@ -1,3 +1,10 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1484
+
+SELECT sell_date , 
+COUNT(DISTINCT product) AS num_sold ,
+GROUP_CONCAT(DISTINCT product ORDER BY product separator ',') AS products 
+FROM Activities 
+GROUP BY sell_date
+ORDER BY sell_date
