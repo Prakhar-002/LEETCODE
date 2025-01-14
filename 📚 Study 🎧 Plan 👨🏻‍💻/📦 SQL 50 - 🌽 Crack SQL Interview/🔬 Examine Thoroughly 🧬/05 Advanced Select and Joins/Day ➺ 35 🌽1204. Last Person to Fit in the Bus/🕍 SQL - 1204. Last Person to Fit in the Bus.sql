1be7178339c8441e9427e15b4716d6ce88c1a6e0
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 16:33:12 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 35 🌽1204. Last Person to Fit in the Bus/🕍 SQL - 1204. Last Person to Fit in the Bus.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 35 🌽1204. Last Person to Fit in the Bus/🕍 SQL - 1204. Last Person to Fit in the Bus.sql
@@ -1,3 +1,11 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1204
+
+SELECT q1.person_name 
+FROM Queue q1
+INNER JOIN Queue q2
+ON q1.turn >= q2.turn 
+GROUP BY q1.turn
+HAVING SUM(q2.weight) <= 1000
+ORDER BY q1.turn DESC LIMIT 1
