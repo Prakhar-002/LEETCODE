99ed6ef139c5bc0af034ebc3b192386a02d72649
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 16:53:16 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/06 Subqueries/Day ➺ 38 🌽626. Exchange Seats/🕍 SQL - 626. Exchange Seats.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/06 Subqueries/Day ➺ 38 🌽626. Exchange Seats/🕍 SQL - 626. Exchange Seats.sql
@@ -1,3 +1,11 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 626
+
+SELECT  
+CASE 
+      WHEN id = (SELECT MAX(id) FROM Seat) AND id % 2 = 1 THEN id
+      WHEN id % 2 = 1 THEN id + 1 else id - 1 END AS id,
+student 
+FROM Seat
+ORDER BY id 
