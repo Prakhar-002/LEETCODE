abe0690e4e8563429f69120af7ed20ff95c2971d
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 16:45:13 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/06 Subqueries/Day ➺ 37 🌽1978. Employees Whose Manager Left the Company/🕍 SQL - 1978. Employees Whose Manager Left the Com.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/06 Subqueries/Day ➺ 37 🌽1978. Employees Whose Manager Left the Company/🕍 SQL - 1978. Employees Whose Manager Left the Com.sql
@@ -1,3 +1,9 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1978
+
+SELECT employee_id 
+FROM Employees 
+WHERE salary < 30000 
+AND manager_id  NOT IN ( SELECT employee_id FROM  Employees )
+ORDER BY employee_id
