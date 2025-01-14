13aa65fbe8d41d14a6a7bafbc3d84703433b4756
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 18:09:54 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/07 Advanced String Functions & Regex & Clause/Day ➺ 47 🌽 176. Second Highest Salary/🕍 SQL - 176. Second Highest Salary.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/07 Advanced String Functions & Regex & Clause/Day ➺ 47 🌽 176. Second Highest Salary/🕍 SQL - 176. Second Highest Salary.sql
@@ -1,4 +1,7 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
- 
+--Todo 💎 QUESTION NUMBER 176
+
+SELECT MAX(salary) AS SecondHighestSalary 
+FROM Employee
+WHERE salary NOT IN (SELECT MAX(salary) FROM Employee)
