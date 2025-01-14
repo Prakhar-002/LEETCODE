be98cd51ece60dd5d6fe8c072707c9b8c87da852
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 16:27:24 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 34 🌽1164. Product Price at a Given Date/🕍 SQL - 1164. Product Price at a Given Date.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 34 🌽1164. Product Price at a Given Date/🕍 SQL - 1164. Product Price at a Given Date.sql
@@ -1,3 +1,26 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1164
+
+SELECT product_id , new_price AS price
+FROM Products 
+WHERE (product_id, change_date) IN 
+(
+      SELECT product_id, MAX(change_date)
+      FROM Products
+      WHERE change_date <= '2019-08-16'
+      GROUP BY product_id
+)
+
+UNION 
+
+SELECT product_id , 10 AS price
+FROM Products 
+WHERE (product_id) NOT IN 
+(
+      SELECT product_id
+      FROM Products
+      WHERE change_date <= '2019-08-16'
+      GROUP BY product_id
+)
+
