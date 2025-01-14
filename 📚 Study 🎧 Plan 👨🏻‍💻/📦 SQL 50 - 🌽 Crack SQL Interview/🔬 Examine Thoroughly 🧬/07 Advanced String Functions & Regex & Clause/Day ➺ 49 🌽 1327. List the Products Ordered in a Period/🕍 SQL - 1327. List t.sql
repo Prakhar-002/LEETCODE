6ecdd024796d4afb939707bb0a639381d32bc4aa
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 19:24:52 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/07 Advanced String Functions & Regex & Clause/Day ➺ 49 🌽 1327. List the Products Ordered in a Period/🕍 SQL - 1327. List t.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/07 Advanced String Functions & Regex & Clause/Day ➺ 49 🌽 1327. List the Products Ordered in a Period/🕍 SQL - 1327. List t.sql
@@ -1,3 +1,11 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1327
+
+SELECT  p.product_name, SUM(o.unit) AS unit
+FROM Products p
+LEFT JOIN Orders o
+ON p.product_id = o.product_id
+WHERE EXTRACT(year_month FROM o.order_date) = 202002
+GROUP BY o.product_id
+HAVING SUM(o.unit) >= 100
