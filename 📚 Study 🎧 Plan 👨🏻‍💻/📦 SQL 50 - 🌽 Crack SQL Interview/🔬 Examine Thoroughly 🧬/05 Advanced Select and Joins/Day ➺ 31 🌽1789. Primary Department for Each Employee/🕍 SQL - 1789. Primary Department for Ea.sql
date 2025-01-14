c260cd58292524dbfc5acd8c2caf3eab7b5677b3
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 15:58:07 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 31 🌽1789. Primary Department for Each Employee/🕍 SQL - 1789. Primary Department for Ea.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 31 🌽1789. Primary Department for Each Employee/🕍 SQL - 1789. Primary Department for Ea.sql
@@ -1,3 +1,15 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 1789
+
+SELECT  employee_id, department_id
+FROM Employee 
+WHERE primary_flag = 'Y'
+GROUP BY employee_id
+
+UNION 
+
+SELECT  employee_id, department_id
+FROM Employee 
+GROUP BY employee_id
+HAVING COUNT(employee_id) = 1
