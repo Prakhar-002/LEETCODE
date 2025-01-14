a146f0362368c3dcb75aaa68e64f7c594a994ff4
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-14 16:11:16 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 33 🌽180. Consecutive Numbers/🕍 SQL - 180. Consecutive Numbers.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/05 Advanced Select and Joins/Day ➺ 33 🌽180. Consecutive Numbers/🕍 SQL - 180. Consecutive Numbers.sql
@@ -1,3 +1,12 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 180
+
+SELECT l1.num AS ConsecutiveNums
+FROM Logs l1, Logs l2, Logs l3
+WHERE l1.id - l2.id = 1
+      AND l2.id - l3.id = 1
+      AND l1.num = l2.num
+      AND l2.num = l3.num
+      AND l1.num = l3.num
+GROUP BY l1.num
