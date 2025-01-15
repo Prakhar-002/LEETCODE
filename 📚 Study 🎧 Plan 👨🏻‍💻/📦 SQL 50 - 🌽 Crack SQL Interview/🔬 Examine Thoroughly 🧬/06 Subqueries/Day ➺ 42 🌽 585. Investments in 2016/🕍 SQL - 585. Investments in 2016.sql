76110d8f52eae4046a454f52f137b3dac4c6299e
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-15 06:52:53 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/06 Subqueries/Day ➺ 42 🌽 585. Investments in 2016/🕍 SQL - 585. Investments in 2016.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/06 Subqueries/Day ➺ 42 🌽 585. Investments in 2016/🕍 SQL - 585. Investments in 2016.sql
@@ -1,3 +1,18 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 585
+
+SELECT ROUND(SUM(tiv_2016), 2) AS tiv_2016 
+FROM Insurance
+WHERE tiv_2015 IN (
+      SELECT tiv_2015 
+      FROM Insurance
+      GROUP BY tiv_2015
+      HAVING COUNT(*) > 1
+)
+AND (lat , lon) IN (
+      SELECT lat, lon 
+      FROM Insurance
+      GROUP BY lat, lon 
+      HAVING COUNT(*) = 1
+)
