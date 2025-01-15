8a93e8210f3d3990a0ae45885cd35c5264845437
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-15 07:03:21 +0530

    🫀 Hard 🫀

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/06 Subqueries/Day ➺ 43 🌽 185. Department Top Three Salaries/🕍 SQL - 185. Department Top Three Salaries.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/06 Subqueries/Day ➺ 43 🌽 185. Department Top Three Salaries/🕍 SQL - 185. Department Top Three Salaries.sql
@@ -1,3 +1,13 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 185
+
+SELECT Department , Employee , Salary FROM
+(
+SELECT d.name AS Department , e.name AS Employee , e.salary  AS Salary,
+DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) AS ranks
+FROM Employee e 
+LEFT JOIN Department d 
+ON e.departmentId = d.id
+) AS temp
+WHERE ranks <= 3
