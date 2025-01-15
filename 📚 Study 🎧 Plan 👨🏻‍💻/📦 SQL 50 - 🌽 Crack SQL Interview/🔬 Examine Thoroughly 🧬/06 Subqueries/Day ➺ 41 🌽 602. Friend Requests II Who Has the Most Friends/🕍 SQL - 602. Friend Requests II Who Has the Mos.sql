d27f244d7e924124bf1dd1ab459397e3b3ad204e
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-15 06:48:08 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/06 Subqueries/Day ➺ 41 🌽 602. Friend Requests II Who Has the Most Friends/🕍 SQL - 602. Friend Requests II Who Has the Mos.sql
+++ 📚 Study 🎧 Plan 👨🏻‍💻/📦 SQL 50 - 🌽 Crack SQL Interview/🔬 Examine Thoroughly 🧬/06 Subqueries/Day ➺ 41 🌽 602. Friend Requests II Who Has the Most Friends/🕍 SQL - 602. Friend Requests II Who Has the Mos.sql
@@ -1,3 +1,19 @@
 --! https://github.com/Prakhar-002/LEETCODE
 
---Todo 💎 QUESTION NUMBER ----
+--Todo 💎 QUESTION NUMBER 602
+
+SELECT id, COUNT(id) AS num FROM
+(
+
+      SELECT requester_id AS id 
+      FROM RequestAccepted
+
+      UNION ALL
+
+      SELECT accepter_id  AS id
+      FROM RequestAccepted
+
+) temp
+GROUP BY id 
+ORDER BY num DESC
+LIMIT 1 
