68a376bab3ca4df27b371719857c9beeb0496987
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-22 19:25:03 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/🩵 NeetCode 150 - 🍇 Blind 75 + 75 problems/🔬 Examine Thoroughly 🧬/01 Arrays & Hashing/Day ➺ 03 🥡 1. Two Sum ☃️ 🍁 🍰 🎲 💖/🍰PYTHON-1-TwoSum.py
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🩵 NeetCode 150 - 🍇 Blind 75 + 75 problems/🔬 Examine Thoroughly 🧬/01 Arrays & Hashing/Day ➺ 03 🥡 1. Two Sum ☃️ 🍁 🍰 🎲 💖/🍰PYTHON-1-TwoSum.py
@@ -13,7 +13,7 @@ class Solution:
                   for j in range(i + 1, len(nums)):
                         if nums[i] + nums[j] == target:
                               return [i, j]
-            return nums
+            return nums 
 
 # ---------------------------------------------
 
