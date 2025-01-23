ea3b02b1caaf17a95cac5f9d8efb7087003fae27
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-23 17:35:06 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/🩵 NeetCode 150 - 🍇 Blind 75 + 75 problems/🔬 Examine Thoroughly 🧬/01 Arrays & Hashing/Day ➺ 03 🥡 1. Two Sum ☃️ 🍁 🍰 🎲 💖/🍰PYTHON-1-TwoSum.py
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🩵 NeetCode 150 - 🍇 Blind 75 + 75 problems/🔬 Examine Thoroughly 🧬/01 Arrays & Hashing/Day ➺ 03 🥡 1. Two Sum ☃️ 🍁 🍰 🎲 💖/🍰PYTHON-1-TwoSum.py
@@ -7,7 +7,7 @@
 
 #? 🧺 Space complexity -> O(1)  
 
-class Solution:
+class Solution: 
       def twoSum(self, nums: List[int], target: int) -> List[int]:
             for i in range(len(nums)):
                   for j in range(i + 1, len(nums)):
