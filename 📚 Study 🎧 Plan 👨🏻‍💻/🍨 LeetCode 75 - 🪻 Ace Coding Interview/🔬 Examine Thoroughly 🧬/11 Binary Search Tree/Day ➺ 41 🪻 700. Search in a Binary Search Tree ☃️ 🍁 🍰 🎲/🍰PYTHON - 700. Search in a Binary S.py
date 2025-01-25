932d6f771eb671ae9356908c5e280362c4e1342b
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-26 03:10:10 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/🍨 LeetCode 75 - 🪻 Ace Coding Interview/🔬 Examine Thoroughly 🧬/11 Binary Search Tree/Day ➺ 41 🪻 700. Search in a Binary Search Tree ☃️ 🍁 🍰 🎲/🍰PYTHON - 700. Search in a Binary S.py
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🍨 LeetCode 75 - 🪻 Ace Coding Interview/🔬 Examine Thoroughly 🧬/11 Binary Search Tree/Day ➺ 41 🪻 700. Search in a Binary Search Tree ☃️ 🍁 🍰 🎲/🍰PYTHON - 700. Search in a Binary S.py
@@ -1,13 +1,23 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER 700
 
-#? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
+#? ⌚ Time complexity ➺ O(h) 👉🏻  h = Height Of Tree
 
 #? 🧺 Space complexity ➺ O(1)
 
-# https://github.com/Prakhar-002/LEETCODE
+class Solution:
+      def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+            cur = root
 
-# ⌚ Time complexity -> O(n) ->  n = len(nums)
+            while cur:
+                  if cur.val == val:  # If the value matches, return the current node
+                        return cur
 
-#  Space complexity -> O(1)
+                  elif cur.val > val:  # If the value is smaller, move to the left subtree
+                        cur = cur.left
+
+                  else:  # If the value is greater, move to the right subtree
+                        cur = cur.right
+
+            return None  # If the value is not found, return None
