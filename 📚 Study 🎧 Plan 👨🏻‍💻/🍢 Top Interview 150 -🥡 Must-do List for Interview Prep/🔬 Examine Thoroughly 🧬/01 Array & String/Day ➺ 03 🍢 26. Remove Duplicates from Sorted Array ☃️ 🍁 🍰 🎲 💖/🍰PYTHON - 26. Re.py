cdbb750a3413fbd66f11a2226ca749fcdc75f9e2
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-01-29 19:49:51 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/🍢 Top Interview 150 -🥡 Must-do List for Interview Prep/🔬 Examine Thoroughly 🧬/01 Array & String/Day ➺ 03 🍢 26. Remove Duplicates from Sorted Array ☃️ 🍁 🍰 🎲 💖/🍰PYTHON - 26. Re.py
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🍢 Top Interview 150 -🥡 Must-do List for Interview Prep/🔬 Examine Thoroughly 🧬/01 Array & String/Day ➺ 03 🍢 26. Remove Duplicates from Sorted Array ☃️ 🍁 🍰 🎲 💖/🍰PYTHON - 26. Re.py
@@ -1,13 +1,22 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER 26
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
 
 #? 🧺 Space complexity ➺ O(1)
 
-# https://github.com/Prakhar-002/LEETCODE
+class Solution:
+      def removeDuplicates(self, nums: list[int]) -> int:
+            # Initialize pointer `i` to track the position of the last unique element
+            i = 0
 
-# ⌚ Time complexity -> O(n) ->  n = len(nums)
+            # Iterate through the array starting from the second element
+            for j in range(1, len(nums)):
+                  # If the current element is not equal to the last unique element
+                  if nums[i] != nums[j]:
+                        nums[i + 1] = nums[j]  # Place the current element in the next position
+                        i += 1  # Increment the pointer `i`
 
-#  Space complexity -> O(1)
+            # Return the new length of the array, which is `i + 1`
+            return i + 1
---
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🍢 Top Interview 150 -🥡 Must-do List for Interview Prep/🔬 Examine Thoroughly 🧬/01 Array & String/Day ➺ 03 🍢 26. Remove Duplicates from Sorted Array ☃️ 🍁 🍰 🎲 💖/🎲CPP - 26. Remo.cpp
@@ -0,0 +1,30 @@
+//! https://github.com/Prakhar-002/LEETCODE
+
+// Todo 💎 QUESTION NUMBER 26
+
+//? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
+
+//? 🧺 Space complexity ➺ O(1)
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+      int removeDuplicates(vector<int>& nums) {
+            // Initialize pointer `i` to track the position of the last unique element
+            int i = 0;
+
+            // Iterate through the array starting from the second element
+            for (int j = 1; j < nums.size(); j++) {
+                  // If the current element is not equal to the last unique element
+                  if (nums[i] != nums[j]) {
+                        nums[i + 1] = nums[j]; // Place the current element in the next position
+                        i++; // Increment the pointer `i`
+                  }
+            }
+
+            // Return the new length of the array, which is `i + 1`
+            return i + 1;
+      }
+};
