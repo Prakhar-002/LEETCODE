68f586a15edf35681e45aa5d1d1c944bfc01e676
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-02-06 12:52:22 +0530

    ☢️ Medium ☢️

---
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🩵 NeetCode 150 - 🍇 Blind 75 + 75 problems/🔬 Examine Thoroughly 🧬/02 Two Pointers/Day ➺ 11 🥡 167. Two Sum II - Input Array Is Sorted ☃️ 🍁 🍰 🎲 💖/☃️JAVASCRIPT - 167. Two Sum II -.js
@@ -0,0 +1,31 @@
+//! https://github.com/Prakhar-002/LEETCODE
+
+// Todo 💎 QUESTION NUMBER 167
+
+//? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
+
+//? 🧺 Space complexity ➺ O(1)
+
+var twoSum = function (numbers, target) {
+      let first = 0
+      let second = numbers.length - 1
+
+      // While first <= second
+      while (first <= second) {
+            // Calculating sum
+            let sum = numbers[first] + numbers[second];
+            // If it is equal to target break
+            if (sum == target) {
+                  break;
+                  // If sum is greater than target shift second pointer to left
+            } else if (sum > target) {
+                  second--;
+                  // If sum is smaller than target shift first pointer to right
+            } else {
+                  first++;
+            }
+      }
+
+      // return array of both index + 1
+      return [first + 1, second + 1];
+};
--- 📚 Study 🎧 Plan 👨🏻‍💻/🩵 NeetCode 150 - 🍇 Blind 75 + 75 problems/🔬 Examine Thoroughly 🧬/02 Two Pointers/Day ➺ 11 🥡 167. Two Sum II - Input Array Is Sorted ☃️ 🍁 🍰 🎲 💖/🍰PYTHON - 167. Two Sum II - Inp.py
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🩵 NeetCode 150 - 🍇 Blind 75 + 75 problems/🔬 Examine Thoroughly 🧬/02 Two Pointers/Day ➺ 11 🥡 167. Two Sum II - Input Array Is Sorted ☃️ 🍁 🍰 🎲 💖/🍰PYTHON - 167. Two Sum II - Inp.py
@@ -1,13 +1,30 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER 167
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
 
 #? 🧺 Space complexity ➺ O(1)
 
-# https://github.com/Prakhar-002/LEETCODE
+class Solution:
+      def twoSum(self, numbers: List[int], target: int) -> List[int]:
+            # taking to pointer a star with 0 and end
+            first = 0
+            second = len(numbers) - 1
 
-# ⌚ Time complexity -> O(n) ->  n = len(nums)
+            # While first <= second
+            while first <= second :
+                  # Calculating sum
+                  sum = numbers[first] + numbers[second]
+                  #  If it is equal to target break
+                  if sum == target:
+                        break
+                  # If sum is greater than target shift second pointer to left
+                  elif sum > target:
+                        second -= 1
+                  # sum is smaller than target shift first pointer to right 
+                  else :
+                        first += 1
 
-#  Space complexity -> O(1)
+            # return array of both index + 1
+            return [first + 1, second + 1]
