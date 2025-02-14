1a793c1149a33a365ed841c51c2c24118acac564
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-02-14 20:20:33 +0530

    ☢️ Medium ☢️

--- 📚 Study 🎧 Plan 👨🏻‍💻/🍨 LeetCode 75 - 🪻 Ace Coding Interview/🔬 Examine Thoroughly 🧬/14 Heap  Priority & Queue/Day ➺ 49 🪻 215. Kth Largest Element in an Array ☃️ 🍁 🍰 🎲/🍰PYTHON - 215. Kth Largest Ele.py
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🍨 LeetCode 75 - 🪻 Ace Coding Interview/🔬 Examine Thoroughly 🧬/14 Heap  Priority & Queue/Day ➺ 49 🪻 215. Kth Largest Element in an Array ☃️ 🍁 🍰 🎲/🍰PYTHON - 215. Kth Largest Ele.py
@@ -1,13 +1,24 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER 215
 
-#? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
+#? ⌚ Time complexity ➺ O(k Log n) 👉🏻  n = len(nums)
 
-#? 🧺 Space complexity ➺ O(1)
+#? 🧺 Space complexity ➺ O(n)
 
-# https://github.com/Prakhar-002/LEETCODE
+import heapq
 
-# ⌚ Time complexity -> O(n) ->  n = len(nums)
+class Solution:
+      def findKthLargest(self, nums, k):
+            # Convert nums into a max heap (negate values to simulate max heap)
+            max_heap = [-n for n in nums]
+            heapq.heapify(max_heap)
 
-#  Space complexity -> O(1)
+            # Extract k-1 elements from the max heap
+            while k > 1:
+                  heapq.heappop(max_heap)
+                  k -= 1
+
+            # Return the k-th largest element (negate back)
+            return -heapq.heappop(max_heap)
+
