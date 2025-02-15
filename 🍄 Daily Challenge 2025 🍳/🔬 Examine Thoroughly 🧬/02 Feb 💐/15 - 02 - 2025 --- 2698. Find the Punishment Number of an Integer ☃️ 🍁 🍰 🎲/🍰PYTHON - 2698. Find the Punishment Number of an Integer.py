35db1cc6afe5405bac78dcb41b9f68f17b951aed
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-02-15 09:19:57 +0530

    ☢️ Medium ☢️

--- 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/02 Feb 💐/15 - 02 - 2025 --- 2698. Find the Punishment Number of an Integer ☃️ 🍁 🍰 🎲/🍰PYTHON - 2698. Find the Punishment Number of an Integer.py
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/02 Feb 💐/15 - 02 - 2025 --- 2698. Find the Punishment Number of an Integer ☃️ 🍁 🍰 🎲/🍰PYTHON - 2698. Find the Punishment Number of an Integer.py
@@ -1,13 +1,41 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER 2698
 
-#? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
+#? ⌚ Time complexity ➺ O(n ^ 2)
 
-#? 🧺 Space complexity ➺ O(1)
+#? 🧺 Space complexity ➺ O(log n)
 
-# https://github.com/Prakhar-002/LEETCODE
+class Solution:
+      def partition(self, i, cur: int, target: int, s: str):
+            """
+            Recursively checks if the number can be partitioned such that the sum of the segments equals the target.
 
-# ⌚ Time complexity -> O(n) ->  n = len(nums)
+            :param i: Current index in the string s
+            :param cur: Current sum of the partitioned numbers
+            :param target: The target sum to achieve
+            :param s: The squared number as a string
+            :return: True if a valid partition exists, otherwise False
+            """
+            # Base case: If we have processed the entire string and sum equals target
+            if i == len(s) and cur == target:
+                  return True
 
-#  Space complexity -> O(1)
+            # Try different partitions from index i to the end of the string
+            for j in range(i, len(s)):
+                  # Recursively check if the remaining part can sum up to target
+                  if self.partition(j + 1, cur + int(s[i : j + 1]), target, s):
+                        return True
+
+            return False
+
+      def punishmentNumber(self, n: int) -> int:
+            punishment = 0
+
+            # Iterate over all numbers from 1 to n
+            for i in range(1, n + 1):
+                  # Check if the square of i can be partitioned to sum to i
+                  if self.partition(0, 0, i, str(i * i)):
+                        punishment += i * i  # Add the squared value to punishment
+
+            return punishment
---
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/02 Feb 💐/15 - 02 - 2025 --- 2698. Find the Punishment Number of an Integer ☃️ 🍁 🍰 🎲/🎲CPP - 2698. Find the Punishment Number of an Integer.cpp
@@ -0,0 +1,52 @@
+//! https://github.com/Prakhar-002/LEETCODE
+
+// Todo 💎 QUESTION NUMBER 2698
+
+//? ⌚ Time complexity ➺ O(n ^ 2)
+
+//? 🧺 Space complexity ➺ O(log n)
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+      /*
+      * Recursively checks if the number can be partitioned such that the sum of segments equals the target.
+      * @param i :      Current index in the string s
+      * @param cur :    Current sum of the partitioned numbers
+      * @param target : The target sum to achieve
+      * @param s :      The squared number as a string
+      * @return       True if a valid partition exists, otherwise False
+      */
+      bool partition(int i, int cur, int target, string s) {
+            // Base case: If we have processed the entire string and sum equals target
+            if (i == s.length() && cur == target) {
+                  return true;
+            }
+
+            // Try different partitions from index i to the end of the string
+            for (int j = i; j < s.length(); j++) {
+                  // Recursively check if the remaining part can sum up to target
+                  if (partition(j + 1, cur + stoi(s.substr(i, j - i + 1)), target, s)) {
+                        return true;
+                  }
+            }
+            return false;
+      }
+
+      int punishmentNumber(int n) {
+            int punishment = 0;
+
+            // Iterate over all numbers from 1 to n
+            for (int i = 1; i <= n; i++) {
+                  // Check if the square of i can be partitioned to sum to i
+                  if (partition(0, 0, i, to_string(i * i))) {
+                        punishment += i * i; // Add the squared value to punishment
+                  }
+            }
+            return punishment;
+      }
+};
