878f60bd6ab90a803ecffbf0ccfdf2911f5c116a
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-03-09 09:36:56 +0530

    ♻️ Easy ♻️

--- 🎭 LEVEL wise que with solution 🎯/♻️ Easy♻️/♻️ Easy 3206. Alternating Groups I ☃️ 🍁 🍰  🎲 💖/🍰PYTHON - 3206. Alternating Groups I.py
+++ 🎭 LEVEL wise que with solution 🎯/♻️ Easy♻️/♻️ Easy 3206. Alternating Groups I ☃️ 🍁 🍰  🎲 💖/🍰PYTHON - 3206. Alternating Groups I.py
@@ -1,6 +1,6 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER 3206
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
 
@@ -11,3 +11,12 @@
 # ⌚ Time complexity -> O(n) ->  n = len(nums)
 
 #  Space complexity -> O(1)
+
+class Solution:
+      def numberOfAlternatingGroups(self, colors: List[int]) -> int:
+            N = len(colors)
+            l = 0
+            curDiff = 0
+            altGrp = 0
+
+            
