d16efe2470d851a53575dfbc673ca4e660766c60
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-03-19 08:20:30 +0530

    ☢️ Medium ☢️

--- 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/03 Mar 🌼/19 - 03 - 2025 --- 3191. Minimum Operations to Make Binary Array Elements Equal to One I ☃️ 🍁 🍰 🎲/🍰PYTHON - 3191. Minimum Operations to Make Bi.py
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/03 Mar 🌼/19 - 03 - 2025 --- 3191. Minimum Operations to Make Binary Array Elements Equal to One I ☃️ 🍁 🍰 🎲/🍰PYTHON - 3191. Minimum Operations to Make Bi.py
@@ -1,13 +1,28 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER 3194
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
 
 #? 🧺 Space complexity ➺ O(1)
 
-# https://github.com/Prakhar-002/LEETCODE
+from typing import List
 
-# ⌚ Time complexity -> O(n) ->  n = len(nums)
+class Solution:
+      def minOperations(self, nums: List[int]) -> int:
+            minOps = 0  # Stores the count of operations needed to make the array valid
 
-#  Space complexity -> O(1)
+            # Iterate over the array up to the third last element
+            for i in range(len(nums) - 2):
+                  # If nums[i] is 0, we need to flip it along with the next two elements
+                  if nums[i] == 0:
+                        minOps += 1  # Increment operation count
+                        nums[i] = 1  # Flip nums[i]
+                        nums[i + 1] = 1 - nums[i + 1]  # Toggle nums[i+1]
+                        nums[i + 2] = 1 - nums[i + 2]  # Toggle nums[i+2]
+
+            # Check if the last two elements are both 1; otherwise, return -1
+            if nums[-1] == 0 or nums[-2] == 0:
+                  return -1  # Impossible to convert array to desired state
+
+            return minOps  # Return the minimum number of operations performed
---
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/03 Mar 🌼/19 - 03 - 2025 --- 3191. Minimum Operations to Make Binary Array Elements Equal to One I ☃️ 🍁 🍰 🎲/🎲CPP - 3191. Minimum Operations to Make Bina.cpp
@@ -0,0 +1,32 @@
+//! https://github.com/Prakhar-002/LEETCODE
+
+// Todo 💎 QUESTION NUMBER 3194
+
+//? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
+
+//? 🧺 Space complexity ➺ O(1)
+
+class Solution {
+public:
+      int minOperations(vector<int>& nums) {
+            int minOps = 0; // Stores the count of operations needed
+
+            // Iterate up to the third last element
+            for (int i = 0; i < nums.size() - 2; i++) {
+                  // If nums[i] is 0, perform an operation
+                  if (nums[i] == 0) {
+                        minOps++; // Increment operation count
+                        nums[i] = 1; // Flip nums[i]
+                        nums[i + 1] = 1 - nums[i + 1]; // Toggle nums[i+1]
+                        nums[i + 2] = 1 - nums[i + 2]; // Toggle nums[i+2]
+                  }
+            }
+
+            // Check if the last two elements are both 1; otherwise, return -1
+            if (nums[nums.size() - 1] == 0 || nums[nums.size() - 2] == 0) {
+                  return -1; // Impossible to make the array valid
+            }
+
+            return minOps; // Return the minimum number of operations performed
+      }
+};
