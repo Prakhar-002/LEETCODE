aa30f68227d9a2e0cc79edbbf178b01d32ad0e55
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-06-10 15:05:20 +0530

    ☢️ Medium ☢️

--- 📜 Daily Challange 💡/05 May 🌈 2024/21 - 05 - 2024 --- 78. Subsets 🍰 🍁 ☃️/🍰PYTHON___78_Subsets.py
+++ 📜 Daily Challange 💡/05 May 🌈 2024/21 - 05 - 2024 --- 78. Subsets 🍰 🍁 ☃️/🍰PYTHON___78_Subsets.py
@@ -1,8 +1,6 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-
-#Todo 📌 QUESTION NUMBER 78 
-
+# Todo 📌 QUESTION NUMBER 78
 
 class Solution:
     def solver(self, arr, idx, temp, result):
@@ -20,3 +18,27 @@ class Solution:
         temp = []
         self.solver(nums, 0, temp, result)
         return result
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subset = []
+        res = []
+
+        def dfs(i):
+            # Base case: Reached the end of the input, add the current subset to the result.
+            if i >= len(nums):
+                res.append(subset[:])  # append a copy, very important!
+                return
+
+            # Include nums[i] in the subset.
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            # Exclude nums[i] from the subset (backtracking).
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)  # start with the first index
+
+        return res
