9039b8e0fb33a195f1e3aeb4cfe02709385b1607
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-06-13 19:35:11 +0530

    ☢️ Medium ☢️

--- 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/06 June 🏕️/13 - 06 - 2025 --- 2616. Minimize the Maximum Difference of Pairs ☃️ 🍁 🍰 🎲/🍰PYTHON - 2616. Minimize the Maximum Difference of Pairs.py
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/06 June 🏕️/13 - 06 - 2025 --- 2616. Minimize the Maximum Difference of Pairs ☃️ 🍁 🍰 🎲/🍰PYTHON - 2616. Minimize the Maximum Difference of Pairs.py
@@ -4,7 +4,7 @@
 
 #? ⌚ Time complexity ➺ O(n*logV+ n*log n) 👉🏻  n = len(nums)
 
-#? 🧺 Space complexity ➺ O(n)
+#? 🧺 Space complexity ➺ O(n) 
 
 class Solution:
       def minimizeMax(self, nums: List[int], p: int) -> int:
