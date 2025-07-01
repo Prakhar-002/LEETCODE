d6bf9386e0d472c1f65d84f2eb5dd9d9df1e2ccc
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-07-01 08:21:44 +0530

    ♻️ Easy ♻️

--- 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/07 July 🍹/01 - 07 - 2025 --- 3330. Find the Original Typed String I ☃️ 🍁 🍰 🎲 💖/🍰PYTHON - 3330. Find the Original Typed String I.py
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/07 July 🍹/01 - 07 - 2025 --- 3330. Find the Original Typed String I ☃️ 🍁 🍰 🎲 💖/🍰PYTHON - 3330. Find the Original Typed String I.py
@@ -10,7 +10,7 @@ class Solution:
       def possibleStringCount(self, word: str) -> int:
             ans, n = 1, len(word)  # Start with one possible string (the original)
 
-            for i in range(1, n):
+            for i in range(1, n): 
                   # If two consecutive characters are the same, we can swap or modify
                   if word[i - 1] == word[i]:
                         ans += 1  # Increase the count of possible strings
