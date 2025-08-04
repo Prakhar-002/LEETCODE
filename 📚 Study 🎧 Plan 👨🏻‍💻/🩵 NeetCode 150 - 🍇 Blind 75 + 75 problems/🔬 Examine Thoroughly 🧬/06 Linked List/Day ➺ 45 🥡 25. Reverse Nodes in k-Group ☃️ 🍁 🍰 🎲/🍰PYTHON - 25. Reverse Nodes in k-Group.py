76b3153f759f406102d94b28920ee4e8c481924a
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-08-04 23:47:32 +0530

    🫀 Hard 🫀

--- 📚 Study 🎧 Plan 👨🏻‍💻/🩵 NeetCode 150 - 🍇 Blind 75 + 75 problems/🔬 Examine Thoroughly 🧬/06 Linked List/Day ➺ 45 🥡 25. Reverse Nodes in k-Group ☃️ 🍁 🍰 🎲/🍰PYTHON - 25. Reverse Nodes in k-Group.py
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🩵 NeetCode 150 - 🍇 Blind 75 + 75 problems/🔬 Examine Thoroughly 🧬/06 Linked List/Day ➺ 45 🥡 25. Reverse Nodes in k-Group ☃️ 🍁 🍰 🎲/🍰PYTHON - 25. Reverse Nodes in k-Group.py
@@ -4,7 +4,7 @@
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = Nodes
 
-#? 🧺 Space complexity ➺ O(1)
+#? 🧺 Space complexity ➺ O(1) 
 
 class Solution:
       def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
