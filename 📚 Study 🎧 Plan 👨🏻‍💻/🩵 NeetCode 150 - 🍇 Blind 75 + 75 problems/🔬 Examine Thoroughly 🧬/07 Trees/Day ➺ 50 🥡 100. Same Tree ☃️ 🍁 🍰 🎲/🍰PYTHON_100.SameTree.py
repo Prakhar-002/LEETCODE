0feb3e36d35b60d1b4b2ddfe6ffa7f838d2f5cae
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-08-05 20:14:10 +0530

    ♻️ Easy ♻️

--- 📚 Study 🎧 Plan 👨🏻‍💻/🩵 NeetCode 150 - 🍇 Blind 75 + 75 problems/🔬 Examine Thoroughly 🧬/07 Trees/Day ➺ 50 🥡 100. Same Tree ☃️ 🍁 🍰 🎲/🍰PYTHON_100.SameTree.py
+++ 📚 Study 🎧 Plan 👨🏻‍💻/🩵 NeetCode 150 - 🍇 Blind 75 + 75 problems/🔬 Examine Thoroughly 🧬/07 Trees/Day ➺ 50 🥡 100. Same Tree ☃️ 🍁 🍰 🎲/🍰PYTHON_100.SameTree.py
@@ -4,7 +4,7 @@ class Solution:
       def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
             if not p and not q:
                   return True
-            
+
             if not p or not q:
                   return False 
             
