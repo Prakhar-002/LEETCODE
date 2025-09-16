229830d348d6636950646d15c5aa6159c3a835a8
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-09-16 10:05:41 +0530

    🫀 Hard 🫀

--- 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/09 Sep 🎃/16 - 09 - 2025 --- 2197. Replace Non-Coprime Numbers in Array ☃️ 🍁 🍰 🎲/🍰PYTHON - 2197. Replace Non-Coprime Numbers in Array.py
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/09 Sep 🎃/16 - 09 - 2025 --- 2197. Replace Non-Coprime Numbers in Array ☃️ 🍁 🍰 🎲/🍰PYTHON - 2197. Replace Non-Coprime Numbers in Array.py
@@ -7,7 +7,7 @@
 #? 🧺 Space complexity ➺ O(n)
 
 from math import gcd
-from typing import List
+from typing import List 
 
 class Solution:
       def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
