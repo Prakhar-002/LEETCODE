01a0db38ea498ed9bda99a43fa858993d7a33131
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-10-02 13:31:23 +0530

    ☢️ Medium ☢️

---
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/10 Oct 🛕/02 - 10 - 2025 --- 3100. Water Bottles II ☃️ 🍁 🍰 🎲/☃️JAVASCRIPT - 3100. Water Bottles II.js
@@ -0,0 +1,34 @@
+//! https://github.com/Prakhar-002/LEETCODE
+
+// Todo 💎 QUESTION NUMBER 3100
+
+//? ⌚ Time complexity ➺ O(sqrt(n)) 👉🏻  n = len(numBottles)
+
+//? 🧺 Space complexity ➺ O(1)
+
+var maxBottlesDrunk = function (numBottles, numExchange) {
+      // Initialize total bottles drunk with the initial full bottles
+      let ans = numBottles;
+
+      // Track how many empty bottles are currently available after drinking
+      let empty = numBottles;
+
+      // While there are enough empty bottles to perform an exchange
+      while (empty >= numExchange) {
+            // Exchange 'numExchange' empties for 1 full bottle and drink it now
+            // This increases the total drunk count by 1
+            ans += 1;
+
+            // Net effect on empties:
+            // - Spend 'numExchange' empty bottles to get 1 full bottle
+            // - Drink that bottle and gain 1 new empty
+            // => empty = empty - numExchange + 1  ==  empty - (numExchange - 1)
+            empty -= (numExchange - 1);
+
+            // The exchange cost increases by 1 after each exchange
+            numExchange += 1;
+      }
+
+      // Return the total number of bottles drunk
+      return ans;
+};
--- 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/10 Oct 🛕/02 - 10 - 2025 --- 3100. Water Bottles II ☃️ 🍁 🍰 🎲/🍰PYTHON - 3100. Water Bottles II.py
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/10 Oct 🛕/02 - 10 - 2025 --- 3100. Water Bottles II ☃️ 🍁 🍰 🎲/🍰PYTHON - 3100. Water Bottles II.py
@@ -1,13 +1,35 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER 3100
 
-#? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
+# ? ⌚ Time complexity ➺ O(sqrt(n)) 👉🏻  n = len(numBottles)
 
-#? 🧺 Space complexity ➺ O(1)
+# ? 🧺 Space complexity ➺ O(1)
 
-# https://github.com/Prakhar-002/LEETCODE
+from typing import List
 
-# ⌚ Time complexity -> O(n) ->  n = len(nums)
+class Solution:
+      def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+            # Initialize total bottles drunk with the initial full bottles
+            ans = numBottles
 
-#  Space complexity -> O(1)
+            # Track how many empty bottles are currently available after drinking
+            empty = numBottles
+
+            # While there are enough empty bottles to perform an exchange
+            while empty >= numExchange:
+                  # Exchange 'numExchange' empties for 1 full bottle and drink it now
+                  # This increases the total drunk count by 1
+                  ans += 1
+
+                  # Net effect on empties:
+                  # - Spend 'numExchange' empty bottles to get 1 full bottle
+                  # - Drink that bottle and gain 1 new empty
+                  # => empty = empty - numExchange + 1  ==  empty - (numExchange - 1)
+                  empty -= numExchange - 1
+
+                  # The exchange cost increases by 1 after each exchange
+                  numExchange += 1
+
+            # Return the total number of bottles drunk
+            return ans
