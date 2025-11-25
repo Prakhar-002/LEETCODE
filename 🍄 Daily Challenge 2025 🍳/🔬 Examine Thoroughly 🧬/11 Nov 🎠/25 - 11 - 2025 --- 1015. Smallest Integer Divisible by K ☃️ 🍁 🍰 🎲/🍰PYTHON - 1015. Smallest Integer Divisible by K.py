badd0e32c059117a4197412ed15cc329ff59ab7b
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-11-25 10:07:07 +0530

    ☢️ Medium ☢️

--- 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/11 Nov 🎠/25 - 11 - 2025 --- 1015. Smallest Integer Divisible by K ☃️ 🍁 🍰 🎲/🍰PYTHON - 1015. Smallest Integer Divisible by K.py
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/11 Nov 🎠/25 - 11 - 2025 --- 1015. Smallest Integer Divisible by K ☃️ 🍁 🍰 🎲/🍰PYTHON - 1015. Smallest Integer Divisible by K.py
@@ -1,13 +1,22 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER Quest Maths L2 Q2
 
-#? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
+#? ⌚ Time complexity ➺ O(k)
 
-#? 🧺 Space complexity ➺ O(1)
+#? 🧺 Space complexity ➺ O(1) 
 
-# https://github.com/Prakhar-002/LEETCODE
+class Solution:
+      def smallestRepunitDivByK(self, k: int) -> int:
+            # If k is divisible by 2 or 5, there is no repunit divisible by k
+            if k % 2 == 0 or k % 5 == 0:
+                  return -1
 
-# ⌚ Time complexity -> O(n) ->  n = len(nums)
+            r = 0  # Remainder variable to build repunits mod k
 
-#  Space complexity -> O(1)
+            # Try repunits of length N from 1 to k
+            for N in range(1, k + 1):
+                  # Update remainder: (previous remainder * 10 + 1) mod k
+                  r = (r * 10 + 1) % k
+                  if r == 0:
+                        return N  # Found repunit divisible by k with length N
