b417114eb97a6d9de069dd6acddab56863827fc0
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-12-03 18:19:52 +0530

    📜 QUESTION 📜️

--- 🏕️ Quest 🧉/🏎️ DSA 💨/🔬 Examine Thoroughly 🧬/Level 4/Counting/Q2. Sort an Array/🎲CPP - Sort an Array.cpp
+++ 🏕️ Quest 🧉/🏎️ DSA 💨/🔬 Examine Thoroughly 🧬/Level 4/Counting/Q2. Sort an Array/🎲CPP - Sort an Array.cpp
@@ -8,19 +8,16 @@
 
 #include <vector>
 #include <algorithm>
-using namespace std;
+using namespace std; 
 
-class Solution
-{
+class Solution {
 public:
-      vector<int> sortArray(vector<int> &nums)
-      {
+      vector<int> sortArray(vector<int>& nums) {
             // let 0th idx val is max and min
             int max = nums[0];
             int min = nums[0];
 
-            for (int num : nums)
-            {
+            for (int num : nums) {
                   // find the max from array
                   max = max > num ? max : num;
                   // find the min from array
@@ -28,40 +25,37 @@ public:
             }
 
             // get the length of our new array
-            int n = max - min;
-            // make the array of out length (n) which will be used to
+            int n = max - min; 
+            // make the array of out length (n) which will be used to 
             // store the frequency of each integer value in `nums`.
             vector<int> arr(n + 1, 0);
 
-            for (int num : nums)
-            {
+            for (int num : nums) {
                   // count the freq of nums's elem
                   arr[num - min]++;
             }
 
             int idx = 0;
 
-            // This is where the actual sorting happens.
-            // `idx` is an index pointer for updating the input array `nums`.
-            // The outer loop iterates through each possible value in the range between `min` and `max`.
-            // The inner while loop runs as long as there are still occurrences (`arr[i] > 0`) of the current number (`min`).
-            // Each time, it assigns the number `min` to `nums[idx]` and increments `idx`.
-            // It also decrements the frequency count `arr[i]`.
+            // This is where the actual sorting happens. 
+            // `idx` is an index pointer for updating the input array `nums`. 
+            // The outer loop iterates through each possible value in the range between `min` and `max`. 
+            // The inner while loop runs as long as there are still occurrences (`arr[i] > 0`) of the current number (`min`). 
+            // Each time, it assigns the number `min` to `nums[idx]` and increments `idx`. 
+            // It also decrements the frequency count `arr[i]`. 
             // After exhausting the occurrences of `min`, the value of `min` is incremented to move to the next number.
 
-            // go through the whole array
-            for (int i = 0; i <= n; i++)
-            {
+            // go through the whole array 
+            for (int i = 0; i <= n; i++) {
                   // up to freq of each elem of our array
-                  while (arr[i] > 0)
-                  {
-                        // assign the value
+                  while (arr[i] > 0) {
+                        // assign the value 
                         nums[idx++] = min;
                         // dec freq
                         arr[i]--;
                   }
 
-                  // inc the value
+                  // inc the value 
                   min++;
             }
 
