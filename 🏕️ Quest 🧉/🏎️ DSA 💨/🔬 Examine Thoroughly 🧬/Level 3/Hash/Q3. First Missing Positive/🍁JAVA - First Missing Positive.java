54f8bc4511e0084d1379d9b26df7d2f74a546012
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-12-03 13:56:03 +0530

    🫀 Hard 🫀

---
+++ 🏕️ Quest 🧉/🏎️ DSA 💨/🔬 Examine Thoroughly 🧬/Level 3/Hash/Q3. First Missing Positive/🍁JAVA - First Missing Positive.java
@@ -0,0 +1,41 @@
+//! https://github.com/Prakhar-002/LEETCODE
+
+class Solution {
+      public int firstMissingPositive(int[] nums) {
+            int n = nums.length;
+
+            for (int i = 0; i < n; i++) {
+                  if (nums[i] < 0) {
+                        // If number is -ve make it 0
+                        nums[i] = 0;
+                  }
+            }
+
+            for (int i = 0; i < n; i++) {
+                  // Take the abs value of every elem of nums
+                  int val = Math.abs(nums[i]);
+                  // If it is btw 1 to len(nums) means in range of possible solution
+                  if (val >= 1 && val <= n) {
+                        // Make the number -ve present at val's original position
+                        // Ex- [1, 2, 3] => actual position is at index val - 1 => 1 : 0, 2 : 1, 3 : 2
+                        // We will make the value -ve for ensuring value present in our array
+                        if (nums[val - 1] > 0) {
+                              nums[val - 1] *= -1;
+                        }
+                        // If it's 0 then assign it with -(len(nums) + 1)
+                        else if (nums[val - 1] == 0) {
+                              nums[val - 1] = -1 * (n + 1);
+                        }
+                  }
+            }
+
+            for (int i = 1; i < n + 1; i++) {
+                  // check if number is not exist in array
+                  if (nums[i - 1] >= 0) {
+                        return i;
+                  }
+            }
+
+            return n + 1;
+      }
+}
--- 🏕️ Quest 🧉/🏎️ DSA 💨/🔬 Examine Thoroughly 🧬/Level 3/Hash/Q3. First Missing Positive/🍰PYTHON - First Missing Positive.py
+++ 🏕️ Quest 🧉/🏎️ DSA 💨/🔬 Examine Thoroughly 🧬/Level 3/Hash/Q3. First Missing Positive/🍰PYTHON - First Missing Positive.py
@@ -1,13 +1,30 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+class Solution:
+      def firstMissingPositive(self, nums: List[int]) -> int:
+            n = len(nums)
+            for i in range(n):
+                  if nums[i] < 0:
+                        # If number is -ve make it 0
+                        nums[i] = 0 
 
-#? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
+            for i in range(n):
+                  # Take the abs value of every elem of nums
+                  abs_val = abs(nums[i])
+                  # If it is btw 1 to len(nums) means in range of possible solution 
+                  if 1 <= abs_val <= n:
+                        # Make the number -ve present at val's original position 
+                        # Ex- [1, 2, 3]  => actual position is at index val - 1 => 1 : 0, 2 : 1, 3 : 2
+                        # We will make the value -ve for ensuring value present in our array
+                        if nums[abs_val - 1] > 0:
+                              nums[abs_val - 1] *= -1
+                        # If it's 0 then assign it with -(len(nums) + 1)
+                        elif nums[abs_val - 1] == 0:
+                              nums[abs_val - 1] = -1 * (n + 1)
 
-#? 🧺 Space complexity ➺ O(1)
+            for i in range(1, n + 1):
+                  # check if number is not exist in array
+                  if nums[i - 1] >= 0:
+                        return i
 
-# https://github.com/Prakhar-002/LEETCODE
-
-# ⌚ Time complexity -> O(n) ->  n = len(nums)
-
-#  Space complexity -> O(1)
+            return n + 1
