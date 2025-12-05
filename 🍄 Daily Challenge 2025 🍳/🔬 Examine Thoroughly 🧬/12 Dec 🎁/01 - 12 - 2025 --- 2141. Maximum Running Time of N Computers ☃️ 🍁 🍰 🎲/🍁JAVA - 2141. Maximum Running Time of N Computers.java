2a5cb4befef9c5a2287346924cad94e2781ed4d9
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-12-05 10:16:45 +0530

    🫀 Hard 🫀

--- 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/12 Dec 🎁/01 - 12 - 2025 --- 2141. Maximum Running Time of N Computers ☃️ 🍁 🍰 🎲/🍁JAVA - 2141. Maximum Running Time of N Computers.java
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/12 Dec 🎁/01 - 12 - 2025 --- 2141. Maximum Running Time of N Computers ☃️ 🍁 🍰 🎲/🍁JAVA - 2141. Maximum Running Time of N Computers.java
@@ -4,7 +4,7 @@
 
 //? ⌚ Time complexity ➺ O(n ^ 2) 👉🏻  n = len(batteries)
 
-//? 🧺 Space complexity ➺ O(1)
+//? 🧺 Space complexity ➺ O(1) 
 
 class Solution {
       public long maxRunTime(int n, int[] batteries) {
