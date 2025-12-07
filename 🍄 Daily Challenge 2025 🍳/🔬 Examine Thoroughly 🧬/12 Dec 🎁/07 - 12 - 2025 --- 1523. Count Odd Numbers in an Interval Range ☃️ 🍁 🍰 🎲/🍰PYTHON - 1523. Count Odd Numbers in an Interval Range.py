4311a22f98a224742a2287e3480681651c29bb76
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-12-07 14:25:59 +0530

    ♻️ Easy ♻️

--- 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/12 Dec 🎁/07 - 12 - 2025 --- 1523. Count Odd Numbers in an Interval Range ☃️ 🍁 🍰 🎲/🍰PYTHON - 1523. Count Odd Numbers in an Interval Range.py
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/12 Dec 🎁/07 - 12 - 2025 --- 1523. Count Odd Numbers in an Interval Range ☃️ 🍁 🍰 🎲/🍰PYTHON - 1523. Count Odd Numbers in an Interval Range.py
@@ -4,7 +4,7 @@
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
 
-#? 🧺 Space complexity ➺ O(1)
+#? 🧺 Space complexity ➺ O(1) 
 
 class Solution:
       def countOdds(self, low: int, high: int) -> int:
