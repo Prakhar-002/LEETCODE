dc958deb4bf9d779bb54a1c075139896f0aa2d24
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-12-09 09:04:14 +0530

    ☢️ Medium ☢️

--- 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/12 Dec 🎁/09 - 12 - 2025 --- 3583. Count Special Triplets ☃️ 🍁 🍰 🎲/🍰PYTHON - 3583. Count Special Triplets.py
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/12 Dec 🎁/09 - 12 - 2025 --- 3583. Count Special Triplets ☃️ 🍁 🍰 🎲/🍰PYTHON - 3583. Count Special Triplets.py
@@ -4,7 +4,7 @@
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
 
-#? 🧺 Space complexity ➺ O(n)
+#? 🧺 Space complexity ➺ O(n) 
 
 class Solution:
       def specialTriplets(self, nums: List[int]) -> int:
