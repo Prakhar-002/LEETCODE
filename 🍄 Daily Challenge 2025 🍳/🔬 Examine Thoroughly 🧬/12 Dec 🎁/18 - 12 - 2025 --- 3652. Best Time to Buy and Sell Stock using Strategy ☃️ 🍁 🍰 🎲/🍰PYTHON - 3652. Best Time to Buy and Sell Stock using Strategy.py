2c4d743ce0c74fba3ced2f997d57c664e9044548
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2025-12-18 17:15:49 +0530

    ☢️ Medium ☢️

--- 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/12 Dec 🎁/18 - 12 - 2025 --- 3652. Best Time to Buy and Sell Stock using Strategy ☃️ 🍁 🍰 🎲/🍰PYTHON - 3652. Best Time to Buy and Sell Stock using Strategy.py
+++ 🍄 Daily Challenge 2025 🍳/🔬 Examine Thoroughly 🧬/12 Dec 🎁/18 - 12 - 2025 --- 3652. Best Time to Buy and Sell Stock using Strategy ☃️ 🍁 🍰 🎲/🍰PYTHON - 3652. Best Time to Buy and Sell Stock using Strategy.py
@@ -4,7 +4,7 @@
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
 
-#? 🧺 Space complexity ➺ O(n)
+#? 🧺 Space complexity ➺ O(n) 
 
 class Solution:
       def maxProfit(self, prices: List[int], strategy: List[int], k: int) -> int:
