07a8cc06e91051aad92a24c571c016046f3c9058
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2026-01-12 15:57:58 +0530

    ☢️ Medium ☢️

--- 🏕️ Quest 🧉/🫕 Software Design 🧬/🔬 Examine Thoroughly 🧬/Level 5/Q3. Subrectangle Queries/🍰PYTHON - Subrectangle Queries.py
+++ 🏕️ Quest 🧉/🫕 Software Design 🧬/🔬 Examine Thoroughly 🧬/Level 5/Q3. Subrectangle Queries/🍰PYTHON - Subrectangle Queries.py
@@ -4,7 +4,7 @@
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(rectangle)
 
-#? 🧺 Space complexity ➺ O(n)
+#? 🧺 Space complexity ➺ O(n) 
 
 class SubrectangleQueries:
       def __init__(self, rectangle):
