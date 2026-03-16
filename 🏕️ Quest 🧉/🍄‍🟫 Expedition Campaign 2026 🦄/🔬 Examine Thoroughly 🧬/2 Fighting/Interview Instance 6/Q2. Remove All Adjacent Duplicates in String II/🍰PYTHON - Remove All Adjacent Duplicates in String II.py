74df96935406b302c44a0a6aeea5c5f45732a48a
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2026-03-16 17:16:10 +0530

    ☢️ Medium ☢️

--- 🏕️ Quest 🧉/🍄‍🟫 Expedition Campaign 2026 🦄/🔬 Examine Thoroughly 🧬/2 Fighting/Interview Instance 6/Q2. Remove All Adjacent Duplicates in String II/🍰PYTHON - Remove All Adjacent Duplicates in String II.py
+++ 🏕️ Quest 🧉/🍄‍🟫 Expedition Campaign 2026 🦄/🔬 Examine Thoroughly 🧬/2 Fighting/Interview Instance 6/Q2. Remove All Adjacent Duplicates in String II/🍰PYTHON - Remove All Adjacent Duplicates in String II.py
@@ -4,7 +4,7 @@
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(s)
 
-#? 🧺 Space complexity ➺ O(n)
+#? 🧺 Space complexity ➺ O(n) 
 
 class Solution:
       def removeDuplicates(self, s: str, k: int) -> str:
