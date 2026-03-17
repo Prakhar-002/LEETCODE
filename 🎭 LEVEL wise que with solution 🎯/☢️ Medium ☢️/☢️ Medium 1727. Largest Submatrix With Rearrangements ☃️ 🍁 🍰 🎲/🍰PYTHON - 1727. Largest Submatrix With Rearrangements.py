e3f892a0f676243ba66d2fb52d1ab171730351fa
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2026-03-17 16:31:30 +0530

    ☢️ Medium ☢️

--- 🎭 LEVEL wise que with solution 🎯/☢️ Medium ☢️/☢️ Medium 1727. Largest Submatrix With Rearrangements ☃️ 🍁 🍰 🎲/🍰PYTHON - 1727. Largest Submatrix With Rearrangements.py
+++ 🎭 LEVEL wise que with solution 🎯/☢️ Medium ☢️/☢️ Medium 1727. Largest Submatrix With Rearrangements ☃️ 🍁 🍰 🎲/🍰PYTHON - 1727. Largest Submatrix With Rearrangements.py
@@ -4,7 +4,7 @@
 
 #? ⌚ Time complexity ➺ O(n * m) 
 
-#? 🧺 Space complexity ➺ O(n)
+#? 🧺 Space complexity ➺ O(n) 
 
 class Solution:
       def largestSubmatrix(self, matrix: List[List[int]]) -> int:
