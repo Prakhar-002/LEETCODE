bbdd36a2be06292f47b156e2a6ec58100e5b9a0a
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2026-03-24 18:42:53 +0530

    ☢️ Medium ☢️

--- 🎭 LEVEL wise que with solution 🎯/☢️ Medium ☢️/☢️ Medium 2906. Construct Product Matrix ☃️ 🍁 🍰 🎲/🍰PYTHON - 2906. Construct Product Matrix.py
+++ 🎭 LEVEL wise que with solution 🎯/☢️ Medium ☢️/☢️ Medium 2906. Construct Product Matrix ☃️ 🍁 🍰 🎲/🍰PYTHON - 2906. Construct Product Matrix.py
@@ -1,6 +1,6 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER 2906
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
 
@@ -11,3 +11,27 @@
 # ⌚ Time complexity -> O(n) ->  n = len(nums)
 
 #  Space complexity -> O(1)
+
+class Solution:
+      def constructProductMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+            ROWS, COLS = len(grid), len(grid[0])
+            MOD = 12345
+
+            product = [[0] * COLS for _ in range(ROWS)]
+
+            suffix = 1
+
+            for i in range(ROWS - 1, -1, -1):
+                  for j in range(COLS - 1, -1, -1):
+                        product[i][j] = suffix
+                        suffix = (suffix * grid[i][j]) % MOD
+
+
+            prefix = 1
+
+            for i in range(ROWS):
+                  for j in range(COLS):
+                        product[i][j] = (product[i][j] * prefix) % MOD
+                        prefix = (prefix * grid[i][j]) % MOD
+
+            return product
