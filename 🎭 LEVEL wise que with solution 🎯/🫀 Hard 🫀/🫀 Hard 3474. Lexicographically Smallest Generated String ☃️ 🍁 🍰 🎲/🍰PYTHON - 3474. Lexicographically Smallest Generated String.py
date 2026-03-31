8aa15bb99f144de289a278c3184362d9fa8b117d
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2026-03-31 14:56:37 +0530

    🫀 Hard 🫀

--- 🎭 LEVEL wise que with solution 🎯/🫀 Hard 🫀/🫀 Hard 3474. Lexicographically Smallest Generated String ☃️ 🍁 🍰 🎲/🍰PYTHON - 3474. Lexicographically Smallest Generated String.py
+++ 🎭 LEVEL wise que with solution 🎯/🫀 Hard 🫀/🫀 Hard 3474. Lexicographically Smallest Generated String ☃️ 🍁 🍰 🎲/🍰PYTHON - 3474. Lexicographically Smallest Generated String.py
@@ -1,6 +1,6 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER 3474
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
 
@@ -11,3 +11,7 @@
 # ⌚ Time complexity -> O(n) ->  n = len(nums)
 
 #  Space complexity -> O(1)
+
+class Solution:
+      def generateString(self, str1: str, str2: str) -> str:
+            
