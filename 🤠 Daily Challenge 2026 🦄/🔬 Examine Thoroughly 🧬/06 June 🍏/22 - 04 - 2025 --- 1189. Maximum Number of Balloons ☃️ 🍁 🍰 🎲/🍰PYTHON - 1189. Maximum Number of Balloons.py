ec9eb11316eca965830b3f7f7e207c5059aea0ca
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2026-06-23 17:07:04 +0530

    ♻️ Easy ♻️

--- 🤠 Daily Challenge 2026 🦄/🔬 Examine Thoroughly 🧬/06 June 🍏/22 - 04 - 2025 --- 1189. Maximum Number of Balloons ☃️ 🍁 🍰 🎲/🍰PYTHON - 1189. Maximum Number of Balloons.py
+++ 🤠 Daily Challenge 2026 🦄/🔬 Examine Thoroughly 🧬/06 June 🍏/22 - 04 - 2025 --- 1189. Maximum Number of Balloons ☃️ 🍁 🍰 🎲/🍰PYTHON - 1189. Maximum Number of Balloons.py
@@ -1,6 +1,6 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER 1189
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
 
@@ -11,3 +11,24 @@
 # ⌚ Time complexity -> O(n) ->  n = len(nums)
 
 #  Space complexity -> O(1)
+
+from typing import Counter
+
+class Solution:
+      def maxNumberOfBalloons(self, text: str) -> int:
+            textCount = Counter(text)
+
+            res = 0
+
+            ballon = {
+                  'b' : 1,
+                  'a' : 1,
+                  'l' : 2,
+                  'o' : 2,
+                  'n' : 1
+            }
+
+            for ch in "balon":
+                  res = min(res, textCount[ch] // ballon[ch])
+
+            return 0
