f4c1ebd97c762308f99a730108c5679b81c8880b
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2026-08-08 22:01:55 +0530

    ♻️ Easy ♻️

--- 🤠 Daily Challenge 2026 🦄/🔬 Examine Thoroughly 🧬/08 August 🍸/06 - 08 - 2025 --- 3345. Smallest Divisible Digit Product I ☃️ 🍁 🍰 🎲/🍰PYTHON - 3345. Smallest Divisible Digit Product I.py
+++ 🤠 Daily Challenge 2026 🦄/🔬 Examine Thoroughly 🧬/08 August 🍸/06 - 08 - 2025 --- 3345. Smallest Divisible Digit Product I ☃️ 🍁 🍰 🎲/🍰PYTHON - 3345. Smallest Divisible Digit Product I.py
@@ -1,6 +1,6 @@
 #! https://github.com/Prakhar-002/LEETCODE
 
-# Todo 💎 QUESTION NUMBER ----
+# Todo 💎 QUESTION NUMBER 3345
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
 
@@ -11,3 +11,5 @@
 # ⌚ Time complexity -> O(n) ->  n = len(nums)
 
 #  Space complexity -> O(1)
+
+
