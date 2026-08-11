2ecfac7a86aae3de23d0107f302149a7034b2508
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2026-08-11 20:12:17 +0530

    ♻️ Easy ♻️

--- 🤠 Daily Challenge 2026 🦄/🔬 Examine Thoroughly 🧬/08 August 🍸/11 - 08 - 2025 --- 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum ☃️ 🍁 🍰 🎲/🍰PYTHON - 2996. Smallest Missing Integer Great.py
+++ 🤠 Daily Challenge 2026 🦄/🔬 Examine Thoroughly 🧬/08 August 🍸/11 - 08 - 2025 --- 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum ☃️ 🍁 🍰 🎲/🍰PYTHON - 2996. Smallest Missing Integer Great.py
@@ -4,7 +4,7 @@
 
 #? ⌚ Time complexity ➺ O(n) 👉🏻  n = len(nums)
 
-#? 🧺 Space complexity ➺ O(n)
+#? 🧺 Space complexity ➺ O(n) 
 
 class Solution:
       def missingInteger(self, nums: list[int]) -> int:
