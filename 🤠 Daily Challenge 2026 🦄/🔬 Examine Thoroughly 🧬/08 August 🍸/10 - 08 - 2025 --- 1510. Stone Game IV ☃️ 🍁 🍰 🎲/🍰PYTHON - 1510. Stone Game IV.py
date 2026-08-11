2b32dfafe1953151dfb0030b24961240715fce5e
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2026-08-11 21:31:41 +0530

    🫀 Hard 🫀

--- 🤠 Daily Challenge 2026 🦄/🔬 Examine Thoroughly 🧬/08 August 🍸/10 - 08 - 2025 --- 1510. Stone Game IV ☃️ 🍁 🍰 🎲/🍰PYTHON - 1510. Stone Game IV.py
+++ 🤠 Daily Challenge 2026 🦄/🔬 Examine Thoroughly 🧬/08 August 🍸/10 - 08 - 2025 --- 1510. Stone Game IV ☃️ 🍁 🍰 🎲/🍰PYTHON - 1510. Stone Game IV.py
@@ -4,7 +4,7 @@
 
 #? ⌚ Time complexity ➺ O(n sqrt(n)) 👉🏻  n = len(nums)
 
-#? 🧺 Space complexity ➺ O(n)
+#? 🧺 Space complexity ➺ O(n) 
 
 class Solution:
       def winnerSquareGame(self, n: int) -> bool:
