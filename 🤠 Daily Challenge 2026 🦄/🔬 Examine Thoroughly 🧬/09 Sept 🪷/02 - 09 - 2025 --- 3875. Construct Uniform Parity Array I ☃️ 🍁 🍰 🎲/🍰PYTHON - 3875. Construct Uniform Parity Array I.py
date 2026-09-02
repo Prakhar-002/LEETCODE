08d4911d60dd922b026335ea9b505d61e215ccde
Prakhar Katiyar <prakhar.katiyar.002@gmail.com>
2026-09-02 21:00:06 +0530

    ♻️ Easy ♻️

--- 🤠 Daily Challenge 2026 🦄/🔬 Examine Thoroughly 🧬/09 Sept 🪷/02 - 09 - 2025 --- 3875. Construct Uniform Parity Array I ☃️ 🍁 🍰 🎲/🍰PYTHON - 3875. Construct Uniform Parity Array I.py
+++ 🤠 Daily Challenge 2026 🦄/🔬 Examine Thoroughly 🧬/09 Sept 🪷/02 - 09 - 2025 --- 3875. Construct Uniform Parity Array I ☃️ 🍁 🍰 🎲/🍰PYTHON - 3875. Construct Uniform Parity Array I.py
@@ -7,7 +7,7 @@
 #? 🧺 Space complexity ➺ O(1)
 
 class Solution:
-      def uniformArray(self, nums1: list[int]) -> bool:
+      def uniformArray(self, nums1: list[int]) -> bool: 
             # If all numbers already share parity (all even or all odd), we can choose nums2[i] = nums1[i].
             # If nums1 has both odds and evens, we can make all elements odd:
             # keep odd numbers as nums1[i], and for any even number at index i,
